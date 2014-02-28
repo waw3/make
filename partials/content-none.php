0815840b0248ede89e4ec86b7e57e0cf6d189cc8
Corey McKrill <cmckrill@jestro.com>
2014-02-27 17:21:32 -0900

    Clean up content-none

--- partials/content-none.php
+++ partials/content-none.php
@@ -1,33 +1,32 @@
 <?php
 /**
- * The template part for displaying a message that posts cannot be found.
- *
- * Learn more: http://codex.wordpress.org/Template_Hierarchy
- *
- * @package _s
+ * @package ttf-start
  */
 ?>
 
-<section class="no-results not-found">
-	<header class="page-header">
-		<h1 class="page-title"><?php _e( 'Nothing Found', '_s' ); ?></h1>
-	</header><!-- .page-header -->
+<article class="no-results not-found">
+	<header class="entry-header">
+		<h1 class="entry-title">
+			<?php _e( 'Nothing Found', 'ttf-start' ); ?>
+		</h1>
+	</header>
 
-	<div class="page-content">
+	<div class="entry-content">
 		<?php if ( is_home() && current_user_can( 'publish_posts' ) ) : ?>
-
-			<p><?php printf( __( 'Ready to publish your first post? <a href="%1$s">Get started here</a>.', '_s' ), esc_url( admin_url( 'post-new.php' ) ) ); ?></p>
-
+		<p>
+			<?php
+			printf(
+				__( 'Ready to publish your first post? <a href="%1$s">Get started here</a>.', 'ttf-start' ),
+				esc_url( admin_url( 'post-new.php' ) )
+			);
+			?>
+		</p>
 		<?php elseif ( is_search() ) : ?>
-
-			<p><?php _e( 'Sorry, but nothing matched your search terms. Please try again with some different keywords.', '_s' ); ?></p>
-			<?php get_search_form(); ?>
-
+		<p><?php _e( 'Sorry, but nothing matched your search terms. Please try again with some different keywords.', 'ttf-start' ); ?></p>
 		<?php else : ?>
-
-			<p><?php _e( 'It seems we can&rsquo;t find what you&rsquo;re looking for. Perhaps searching can help.', '_s' ); ?></p>
-			<?php get_search_form(); ?>
-
+		<p><?php _e( 'It seems we can&rsquo;t find what you&rsquo;re looking for. Perhaps searching can help.', 'ttf-start' ); ?></p>
 		<?php endif; ?>
-	</div><!-- .page-content -->
-</section><!-- .no-results -->
+
+		<?php get_search_form(); ?>
+	</div>
+</article>
