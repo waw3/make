d06327e8c384385d7660570f3561eb60ce8ac159
Corey McKrill <cmckrill@jestro.com>
2014-02-27 16:26:14 -0900

    Clean up single and page

--- single.php
+++ single.php
@@ -1,32 +1,29 @@
 <?php
 /**
- * The Template for displaying all single posts.
- *
- * @package _s
+ * @package ttf-start
  */
 
-get_header(); ?>
+get_header();
+?>
 
-	<div id="primary" class="content-area">
-		<main id="main" class="site-main" role="main">
+<?php if ( have_posts() ) : ?>
 
-		<?php while ( have_posts() ) : the_post(); ?>
+	<?php while ( have_posts() ) : the_post(); ?>
 
-			<?php get_template_part( 'content', 'single' ); ?>
+		<?php get_template_part( 'partials/content', 'single' ); ?>
 
-			<?php _s_post_nav(); ?>
+		<?php _s_post_nav(); ?>
 
-			<?php
-				// If comments are open or we have at least one comment, load up the comment template
-				if ( comments_open() || '0' != get_comments_number() ) :
-					comments_template();
-				endif;
-			?>
+		<?php
+		// If comments are open or we have at least one comment, load up the comment template
+		if ( comments_open() || '0' != get_comments_number() ) :
+			comments_template();
+		endif;
+		?>
 
-		<?php endwhile; // end of the loop. ?>
+	<?php endwhile; ?>
 
-		</main><!-- #main -->
-	</div><!-- #primary -->
+<?php endif; ?>
 
 <?php get_sidebar(); ?>
 <?php get_footer(); ?>
