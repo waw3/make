15d962dc33fc666d4d3dff5008e2fe7ce8797213
Corey McKrill <cmckrill@jestro.com>
2014-02-27 17:23:56 -0900

    Rename _entry- partials to entry-

--- partials/content-page.php
+++ partials/content-page.php
@@ -6,11 +6,11 @@
 
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
 	<header class="entry-header">
-		<?php get_template_part( 'partials/_entry', 'title' ); ?>
+		<?php get_template_part( 'partials/entry', 'title' ); ?>
 	</header>
 
 	<div class="entry-content">
 		<?php the_content(); ?>
-		<?php get_template_part( 'partials/_entry', 'pagination' ); ?>
+		<?php get_template_part( 'partials/entry', 'pagination' ); ?>
 	</div>
 </article>
--- partials/content-single.php
+++ partials/content-single.php
@@ -6,16 +6,16 @@
 
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
 	<header class="entry-header">
-		<?php get_template_part( 'partials/_entry', 'title' ); ?>
-		<?php get_template_part( 'partials/_entry', 'date' ); ?>
+		<?php get_template_part( 'partials/entry', 'title' ); ?>
+		<?php get_template_part( 'partials/entry', 'date' ); ?>
 	</header>
 
 	<div class="entry-content">
 		<?php the_content(); ?>
-		<?php get_template_part( 'partials/_entry', 'pagination' ); ?>
+		<?php get_template_part( 'partials/entry', 'pagination' ); ?>
 	</div>
 
 	<footer class="entry-footer">
-		<?php get_template_part( 'partials/_entry', 'taxonomy' ); ?>
+		<?php get_template_part( 'partials/entry', 'taxonomy' ); ?>
 	</footer>
 </article>
