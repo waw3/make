e80c7bd0fedb9948507e8b69d404d308c639c6a7
Corey McKrill <cmckrill@jestro.com>
2014-02-28 17:05:18 -0900

    Add attachment and content-attachment

---
+++ attachment.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * @package ttf-start
+ */
+
+get_header();
+?>
+
+<?php if ( have_posts() ) : ?>
+
+	<?php while ( have_posts() ) : the_post(); ?>
+
+		<?php get_template_part( 'partials/content', 'attachment' ); ?>
+
+		<?php
+		// If comments are open or we have at least one comment, load up the comment template
+		if ( comments_open() || '0' != get_comments_number() ) :
+			comments_template();
+		endif;
+		?>
+
+	<?php endwhile; ?>
+
+<?php endif; ?>
+
+<?php get_sidebar(); ?>
+<?php get_footer(); ?>
---
+++ partials/content-attachment.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * @package ttf-start
+ */
+?>
+
+<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+	<header class="entry-header">
+		<?php get_template_part( 'partials/entry', 'title' ); ?>
+	</header>
+
+	<div class="entry-content">
+		<?php the_content(); ?>
+	</div>
+</article>
