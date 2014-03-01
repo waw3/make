ed889e6b521350e2835c117117206e63e7a17613
Corey McKrill <cmckrill@jestro.com>
2014-02-28 15:58:02 -0900

    Clean up 404

--- 404.php
+++ 404.php
@@ -1,56 +1,64 @@
 <?php
 /**
- * The template for displaying 404 pages (Not Found).
- *
- * @package _s
+ * @package ttf-start
  */
 
-get_header(); ?>
+// Get current URL
+$current_url = '';
+if ( isset( $_SERVER['SERVER_NAME'] ) && isset( $_SERVER['REQUEST_URI'] ) ) :
+	$protocol = ( is_ssl() ) ? 'https://' : 'http://';
+	$current_url = $protocol . $_SERVER['SERVER_NAME'] . $_SERVER['REQUEST_URI'];
+endif;
 
-	<div id="primary" class="content-area">
-		<main id="main" class="site-main" role="main">
+get_header();
+?>
 
-			<section class="error-404 not-found">
-				<header class="page-header">
-					<h1 class="page-title"><?php _e( 'Oops! That page can&rsquo;t be found.', '_s' ); ?></h1>
-				</header><!-- .page-header -->
+<article class="error-404 not-found">
+	<header class="entry-header">
+		<h1 class="entry-title">
+			<?php _e( 'Oops! That page can&rsquo;t be found.', 'ttf-start' ); ?>
+		</h1>
+	</header>
 
-				<div class="page-content">
-					<p><?php _e( 'It looks like nothing was found at this location. Maybe try one of the links below or a search?', '_s' ); ?></p>
+	<div class="entry-content">
+		<p>
+			<?php
+			printf(
+				__( 'Nothing was found at this location: %s', 'ttf-start' ),
+				'<br /><span class="current-url">' . esc_url( $current_url ) . '</span>'
+			);
+			?>
+		</p>
+		<p>
+			<?php _e( 'Maybe try one of the links below or a search?', 'ttf-start' ); ?>
+		</p>
 
-					<?php get_search_form(); ?>
+		<?php get_search_form(); ?>
 
-					<?php the_widget( 'WP_Widget_Recent_Posts' ); ?>
+		<?php the_widget( 'WP_Widget_Recent_Posts' ); ?>
 
-					<?php if ( _s_categorized_blog() ) : // Only show the widget if site has multiple categories. ?>
-					<div class="widget widget_categories">
-						<h2 class="widgettitle"><?php _e( 'Most Used Categories', '_s' ); ?></h2>
-						<ul>
-						<?php
-							wp_list_categories( array(
-								'orderby'    => 'count',
-								'order'      => 'DESC',
-								'show_count' => 1,
-								'title_li'   => '',
-								'number'     => 10,
-							) );
-						?>
-						</ul>
-					</div><!-- .widget -->
-					<?php endif; ?>
+		<?php the_widget( 'WP_Widget_Archives', 'dropdown=1' ); ?>
 
-					<?php
-					/* translators: %1$s: smiley */
-					$archive_content = '<p>' . sprintf( __( 'Try looking in the monthly archives. %1$s', '_s' ), convert_smilies( ':)' ) ) . '</p>';
-					the_widget( 'WP_Widget_Archives', 'dropdown=1', "after_title=</h2>$archive_content" );
-					?>
+		<?php if ( ttf_categorized_blog() ) : ?>
+		<div class="widget widget_categories">
+			<h4 class="widgettitle"><?php _e( 'Most Used Categories', 'ttf-start' ); ?></h4>
+			<ul>
+				<?php
+				wp_list_categories( array(
+					'orderby'    => 'count',
+					'order'      => 'DESC',
+					'show_count' => 1,
+					'title_li'   => '',
+					'number'     => 10,
+				) );
+				?>
+			</ul>
+		</div>
+		<?php endif; ?>
 
-					<?php the_widget( 'WP_Widget_Tag_Cloud' ); ?>
+		<?php the_widget( 'WP_Widget_Tag_Cloud' ); ?>
+	</div>
+</article>
 
-				</div><!-- .page-content -->
-			</section><!-- .error-404 -->
-
-		</main><!-- #main -->
-	</div><!-- #primary -->
-
+<?php get_sidebar(); ?>
 <?php get_footer(); ?>
