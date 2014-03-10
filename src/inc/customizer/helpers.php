6499bb3598a26c40de4f748d9263ac5bcab0ef2f
Corey McKrill <cmckrill@jestro.com>
2014-03-10 14:51:24 -0800

    Remove customizer debug function from helpers

--- src/inc/customizer/helpers.php
+++ src/inc/customizer/helpers.php
@@ -3,14 +3,6 @@
  * @package ttf-one
  */
 
-/**
- * Temporary function for debugging global options
- */
-function ttf_one_customizer_options_dump() {
-	$mods = get_option( 'theme_mods_ttf-one' );
-	var_dump( $mods );
-}
-
 if ( ! function_exists( 'ttf_one_sanitize_choice' ) ) :
 /**
  * Sanitize a value from a list of allowed values.
