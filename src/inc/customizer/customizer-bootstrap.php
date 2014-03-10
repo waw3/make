97cbc4aa61356812122909d37d53d0dc01853e11
Corey McKrill <cmckrill@jestro.com>
2014-03-10 11:20:45 -0800

    Hook up customizer init function

--- src/inc/customizer/customizer-bootstrap.php
+++ src/inc/customizer/customizer-bootstrap.php
@@ -6,16 +6,18 @@
 function ttf_one_customizer_init() {
 	$path = '/inc/customizer/';
 
+	// Only load these on the backend
 	if ( is_admin() ) {
 		require_once( get_template_directory() . $path . 'customizer.php' );
 
 		add_action( 'admin_enqueue_scripts', 'ttf_one_customizer_admin_scripts' );
 	}
 
+	// Always load these
 	require_once( get_template_directory() . $path . 'customizer.php' );
 }
 
-add_action();
+add_action( 'after_setup_theme', 'ttf_one_customizer_init' );
 
 function ttf_one_customizer_admin_scripts() {
 	$path = '/inc/customizer/';
