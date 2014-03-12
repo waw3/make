5f605ab35fe2cbf53c0a2b03a69083078bdc90d7
Corey McKrill <cmckrill@jestro.com>
2014-03-12 11:57:41 -0800

    Customizer: first attempt at control visibility toggling
    It's not working yet :(

--- src/inc/customizer/js/customizer.js
+++ src/inc/customizer/js/customizer.js
@@ -2,21 +2,45 @@
  * @package ttf-one
  */
 
-/**
- * Theme Customizer enhancements for a better user experience.
- *
- * Contains handlers to make Theme Customizer preview reload some changes asynchronously.
- */
 ( function( $ ) {
-	// Site title and description.
-	wp.customize( 'blogname', function( value ) {
+	var api = wp.customize;
+
+	/**
+	 * Asynchronous updating
+	 */
+	// Site Title
+	api( 'blogname', function( value ) {
 		value.bind( function( to ) {
 			$( '.site-title a' ).text( to );
 		} );
 	} );
-	wp.customize( 'blogdescription', function( value ) {
+	// Tagline
+	api( 'blogdescription', function( value ) {
 		value.bind( function( to ) {
 			$( '.site-description' ).text( to );
 		} );
 	} );
+
+	/**
+	 * Visibility toggling for some controls
+	 */
+	$.each({
+		'ttf-one_background-image': {
+			controls: [ 'ttf-one_background-size', 'ttf-one_background-repeat', 'ttf-one_background-position', 'ttf-one_background-attachment' ],
+			callback: function( to ) { return !! to; }
+		}
+	}, function( settingId, o ) {
+		api( settingId, function( setting ) {
+			$.each( o.controls, function( i, controlId ) {
+				api.control( controlId, function( control ) {
+					var visibility = function( to ) {
+						control.container.toggle( o.callback( to ) );
+					};
+
+					visibility( setting.get() );
+					setting.bind( visibility );
+				});
+			});
+		});
+	});
 } )( jQuery );
