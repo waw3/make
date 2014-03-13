0d817fd6be1a2d45545e137e360d3092359fdf96
Zack Tollman <tollmanz@gmail.com>
2014-03-13 09:51:41 -0700

    Add colors implementation scaffolding.

--- src/inc/customizer/helpers.php
+++ src/inc/customizer/helpers.php
@@ -168,6 +168,30 @@ endif;
 
 add_filter( 'ttf_one_css', 'ttf_one_display_background' );
 
+if ( ! function_exists( 'ttf_one_display_colors' ) ) :
+/**
+ * Write the CSS to implement the color options.
+ *
+ * @since  1.0.0.
+ *
+ * @param  string    $css    The current CSS.
+ * @return string            The modified CSS.
+ */
+function ttf_one_display_colors( $css ) {
+	$color_primary   = maybe_hash_hex_color( get_theme_mod( 'color-primary', '#ffffff' ) );
+	$color_secondary = maybe_hash_hex_color( get_theme_mod( 'color-secondary', '#ffffff' ) );
+	$color_text      = maybe_hash_hex_color( get_theme_mod( 'color-text', '#ffffff' ) );
+	$color_accent    = maybe_hash_hex_color( get_theme_mod( 'color-accent', '#ffffff' ) );
+
+	// All variables are escaped at this point
+	$css .= 'a{color:' . $color_primary . ';}button{color:' . $color_primary . ';}p{color:' . $color_text . ';}';
+
+	return $css;
+}
+endif;
+
+add_filter( 'ttf_one_css', 'ttf_one_display_colors' );
+
 if ( ! function_exists( 'ttf_one_css_fonts' ) ) :
 /**
  * Build the CSS rules for the custom fonts
