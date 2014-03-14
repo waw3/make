6a349c21b74dd4aeaa2acd871c37568e72c0cf28
scottrrollo <scottrrollo@gmail.com>
2014-03-15 10:22:54 +1100

    Styling sticky posts.

--- src/partials/entry-date.php
+++ src/partials/entry-date.php
@@ -5,7 +5,10 @@
 ?>
 
 <?php if ( ! is_page() ) : ?>
-<time class="entry-date" datetime="<?php the_time( 'c' ); ?>">
-	<?php echo get_the_date(); ?>
-</time>
+	<time class="entry-date" datetime="<?php the_time( 'c' ); ?>">
+		<?php echo get_the_date(); ?>
+	</time>
+	<?php if ( is_sticky() ) : ?>
+		<span class="sticky-post-label"><?php _e( 'Featured', 'ttf-one' ); ?></span>
+	<?php endif; ?>
 <?php endif; ?>
