8257eb26669013d58c9ae569a799c4d9f57c61d3
Zack Tollman <tollmanz@gmail.com>
2014-03-17 10:15:03 -0700

    Port the builder menu.

---
+++ src/inc/builder/templates/menu.php
@@ -0,0 +1,29 @@
+<?php $class = ( 'c' === get_user_setting( 'ttfonemt' . get_the_ID() ) ) ? 'closed' : 'opened'; ?>
+<div class="ttf-one-menu ttf-one-menu-<?php echo esc_attr( $class ); ?>" id="ttf-one-menu">
+	<div class="ttf-one-menu-pane">
+		<ul class="ttf-one-menu-list">
+			<?php foreach ( ttf_one_get_builder()->get_menu_items() as $id => $item ) : ?>
+				<li class="ttf-one-menu-list-item">
+					<a href="#" title="<?php esc_attr_e( 'Add', 'ttf-one' ); ?>" class="ttf-one-menu-list-item-link" id="ttf-one-menu-list-item-link-<?php echo esc_attr( $id ); ?>" data-section="<?php echo esc_attr( $id ); ?>">
+						<div class="ttf-one-menu-list-item-link-icon-wrapper clear">
+							<span class="ttf-one-menu-list-item-link-icon"></span>
+						</div>
+						<div class="section-type-description">
+							<h4>
+								<?php echo esc_html( $item['label'] ); ?>
+							</h4>
+							<p>
+								<?php echo esc_html( $item['description'] ); ?>
+							</p>
+						</div>
+					</a>
+				</li>
+			<?php endforeach; ?>
+		</ul>
+	</div>
+	<div class="ttf-one-menu-tab">
+		<a href="#" class="ttf-one-menu-tab-link">
+			<span><?php _e( 'Add New Section', 'ttf-one' ); ?></span>
+		</a>
+	</div>
+</div>
