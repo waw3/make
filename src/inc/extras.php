fdfbbbecddba7fdf7cc75849edfcca2a2001ee77
Corey McKrill <cmckrill@jestro.com>
2014-03-18 13:16:09 -0800

    Tweak PHPDocs

--- src/inc/extras.php
+++ src/inc/extras.php
@@ -1,9 +1,5 @@
 <?php
 /**
- * Custom functions that act independently of the theme templates
- *
- * Eventually, some of the functionality here could be replaced by core features
- *
  * @package ttf-one
  */
 
--- src/inc/template-tags.php
+++ src/inc/template-tags.php
@@ -1,9 +1,5 @@
 <?php
 /**
- * Custom template tags for this theme.
- *
- * Eventually, some of the functionality here could be replaced by core features.
- *
  * @package ttf-one
  */
 
