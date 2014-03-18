d9408244a9bf5181cab4802e84d42f5ba9dd001d
scottrrollo <scottrrollo@gmail.com>
2014-03-19 10:52:41 +1100

    Wrapping post date in link. Styling comments.

--- src/partials/entry-date.php
+++ src/partials/entry-date.php
@@ -6,6 +6,8 @@
 
 <?php if ( ! is_page() ) : ?>
 	<time class="entry-date" datetime="<?php the_time( 'c' ); ?>">
+	<?php if ( ! is_singular() ) : ?><a href="<?php the_permalink(); ?>" rel="bookmark"><?php endif; ?>
 		<?php echo get_the_date(); ?>
+	<?php if ( ! is_singular() ) : ?></a><?php endif; ?>
 	</time>
 <?php endif; ?>
