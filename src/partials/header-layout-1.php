0a4bd52d4037a899a1599b3a70234f81d2c09a99
Corey McKrill <cmckrill@jestro.com>
2014-03-19 11:57:35 -0800

    Header: add .right-content class if social/search enabled

--- src/partials/header-layout-1.php
+++ src/partials/header-layout-1.php
@@ -5,22 +5,25 @@
 ?>
 
 <header id="site-header" class="site-header site-header-1" role="banner">
-	<div class="sub-header">
+	<?php // Sub Header elements
+	$header_text = get_theme_mod( 'header-text', false );
+	$social_links = ttf_one_get_social_links();
+	$show_social = (int) get_theme_mod( 'header-show-social', ttf_one_get_default( 'header-show-social' ) );
+	$show_search = (int) get_theme_mod( 'header-show-search', ttf_one_get_default( 'header-show-search' ) );
+	$subheader_class = ( 1 === $show_social || 1 === $show_search ) ? ' right-content' : ''; ?>
+	<div class="sub-header<?php echo esc_attr( $subheader_class ); ?>">
 		<div class="container">
 			<?php // Header text
-			$header_text = get_theme_mod( 'header-text', false );
 			if ( ! empty( $header_text ) ) : ?>
 			<span class="sub-header-content">
 				<?php echo ttf_one_sanitize_text( $header_text ); ?>
 			</span>
 			<?php endif; ?>
 			<?php // Search form
-			if ( 1 === (int) get_theme_mod( 'header-show-search', ttf_one_get_default( 'header-show-search' ) ) ) :
+			if ( 1 === $show_search ) :
 				get_search_form();
 			endif; ?>
 			<?php // Social links
-			$social_links = ttf_one_get_social_links();
-			$show_social = (int) get_theme_mod( 'header-show-social', ttf_one_get_default( 'header-show-social' ) );
 			if ( ! empty ( $social_links ) && 1 === $show_social ) : ?>
 			<ul class="social-links header-social-links">
 				<?php foreach ( $social_links as $key => $link ) : ?>
