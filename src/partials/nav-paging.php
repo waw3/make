6f4cb28ea26d20b8259ff470b82aef9f58e93bd5
scottrrollo <scottrrollo@gmail.com>
2014-03-20 15:42:47 +1100

    Styling post pagination.

--- src/partials/nav-paging.php
+++ src/partials/nav-paging.php
@@ -10,13 +10,13 @@
 	<div class="nav-links">
 		<?php if ( get_previous_posts_link() ) : ?>
 		<div class="nav-previous">
-			<?php previous_posts_link( __( '<span class="meta-nav">&larr;</span> Newer posts', 'ttf-one' ) ); ?>
+			<?php previous_posts_link( __( 'Newer posts', 'ttf-one' ) ); ?>
 		</div>
 		<?php endif; ?>
 
 		<?php if ( get_next_posts_link() ) : ?>
 		<div class="nav-next">
-			<?php next_posts_link( __( 'Older posts <span class="meta-nav">&rarr;</span>', 'ttf-one' ) ); ?>
+			<?php next_posts_link( __( 'Older posts', 'ttf-one' ) ); ?>
 		</div>
 		<?php endif; ?>
 	</div>
