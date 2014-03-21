8890ee76ec4108a97ebbd800fcd08eff369e1d33
Zack Tollman <tollmanz@gmail.com>
2014-03-21 15:16:12 -0700

    Add basic gallery inputs.

--- src/inc/builder/sections/builder-templates/gallery.php
+++ src/inc/builder/sections/builder-templates/gallery.php
@@ -1,5 +1,30 @@
 <?php ttf_one_load_section_header(); ?>
 <?php global $ttf_one_section_data, $ttf_one_is_js_template; ?>
 <?php $section_name = ttf_one_get_section_name( $ttf_one_section_data, $ttf_one_is_js_template ); ?>
-	<input type="hidden" class="ttf-one-section-state" name="<?php echo $section_name; ?>[state]" value="<?php if ( isset( $ttf_one_section_data['data']['state'] ) ) echo esc_attr( $ttf_one_section_data['data']['state'] ); else echo 'open'; ?>" />
+
+<label for="ttf-one-gallery-columns"><?php _e( 'Columns', 'ttf-one' ); ?></label>
+<select id="ttf-one-gallery-columns">
+	<option value="1">1</option>
+	<option value="2">2</option>
+	<option value="3">3</option>
+	<option value="4">4</option>
+</select>
+
+<label for="ttf-one-gallery-captions"><?php _e( 'Caption Style', 'ttf-one' ); ?></label>
+<select id="ttf-one-gallery-captions">
+	<option value="none"><?php echo esc_html( __( 'None', 'ttf-one' ) ); ?></option>
+	<option value="basic"><?php echo esc_html( __( 'Basic', 'ttf-one' ) ); ?></option>
+	<option value="fancy"><?php echo esc_html( __( 'Fancy', 'ttf-one' ) ); ?></option>
+</select>
+
+<div class="ttf-one-gallery-background">
+	<a href="#"><?php _e( 'Set a background image or color', 'ttf-one' ); ?></a>
+</div>
+
+<div class="ttf-one-gallery-add-item">
+	<a href="#"><?php _e( 'Add a new gallery item', 'ttf-one' ); ?></a>
+</div>
+
+<input type="hidden" class="ttf-one-section-state" name="<?php echo $section_name; ?>[state]" value="<?php if ( isset( $ttf_one_section_data['data']['state'] ) ) echo esc_attr( $ttf_one_section_data['data']['state'] ); else echo 'open'; ?>" />
+
 <?php ttf_one_load_section_footer(); ?>
