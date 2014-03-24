2f7eae0e5db5449d1df37c2d9062784fd6f3a7d3
Zack Tollman <tollmanz@gmail.com>
2014-03-24 08:57:20 -0700

    Activate sections in the order that they appear.

--- src/inc/builder/sections/section-definitions.php
+++ src/inc/builder/sections/section-definitions.php
@@ -40,10 +40,10 @@ class TTF_One_Section_Definitions {
 	 * @return TTF_One_Section_Definitions
 	 */
 	public function __construct() {
-		$this->register_blank_section();
-		$this->register_gallery_section();
 		$this->register_text_section();
 		$this->register_banner_section();
+		$this->register_gallery_section();
+		$this->register_blank_section();
 	}
 
 	/**
