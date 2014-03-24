e2e9e69aa3783f074e1ff3411ad91750938ca701
Zack Tollman <tollmanz@gmail.com>
2014-03-24 13:49:42 -0700

    Add banner boilerplate.

--- src/inc/builder/sections/builder-templates/banner.php
+++ src/inc/builder/sections/builder-templates/banner.php
@@ -1,2 +1,6 @@
 <?php ttf_one_load_section_header(); ?>
+<?php global $ttf_one_section_data, $ttf_one_is_js_template; ?>
+<?php $section_name = ttf_one_get_section_name( $ttf_one_section_data, $ttf_one_is_js_template ); ?>
+
+<input type="hidden" class="ttf-one-section-state" name="<?php echo $section_name; ?>[state]" value="<?php if ( isset( $ttf_one_section_data['data']['state'] ) ) echo esc_attr( $ttf_one_section_data['data']['state'] ); else echo 'open'; ?>" />
 <?php ttf_one_load_section_footer(); ?>
