f6889dc829f1214c16e9cc35e0e427593a33cc58
Zack Tollman <tollmanz@gmail.com>
2014-03-24 11:34:50 -0700

    Add method for appending events and function to view.

--- src/inc/builder/sections/js/section-behaviors.js
+++ src/inc/builder/sections/js/section-behaviors.js
@@ -4,5 +4,17 @@ var oneApp = oneApp || {}, $oneApp = $oneApp || jQuery(oneApp);
 (function (window, $, _, oneApp) {
 	'use strict';
 
-	$oneApp.on('viewInit', function(evt, view){});
+	$oneApp.on('viewInit', function(evt, view){
+		var sectionType = view.model.get('sectionType');
+
+		// Append the additional event to the view
+		view.events = _.extend(view.events, {
+			'click .ttf-one-gallery-add-item a': 'addItem'
+		});
+
+		// Append the needed function to the view
+		view.addItem = function(evt){
+			evt.preventDefault();
+		}
+	});
 })(window, jQuery, _, oneApp);
