10a2e3d4d25ce58e5fec581adad9f8a9e8a2af44
Zack Tollman <tollmanz@gmail.com>
2014-03-24 09:38:37 -0700

    Add item details to gallery template.

--- src/inc/builder/sections/builder-templates/gallery.php
+++ src/inc/builder/sections/builder-templates/gallery.php
@@ -12,12 +12,14 @@
 	</select>
 </div>
 
-<label for="ttf-one-gallery-captions"><?php _e( 'Caption Style', 'ttf-one' ); ?></label>
-<select id="ttf-one-gallery-captions">
-	<option value="none"><?php echo esc_html( __( 'None', 'ttf-one' ) ); ?></option>
-	<option value="basic"><?php echo esc_html( __( 'Basic', 'ttf-one' ) ); ?></option>
-	<option value="fancy"><?php echo esc_html( __( 'Fancy', 'ttf-one' ) ); ?></option>
-</select>
+<div class="ttf-one-captions-select">
+	<label for="ttf-one-gallery-captions"><?php _e( 'Caption Style', 'ttf-one' ); ?></label>
+	<select id="ttf-one-gallery-captions">
+		<option value="none"><?php echo esc_html( __( 'None', 'ttf-one' ) ); ?></option>
+		<option value="basic"><?php echo esc_html( __( 'Basic', 'ttf-one' ) ); ?></option>
+		<option value="fancy"><?php echo esc_html( __( 'Fancy', 'ttf-one' ) ); ?></option>
+	</select>
+</div>
 
 <div class="ttf-one-gallery-background">
 	<a href="#"><?php _e( 'Set a background image or color', 'ttf-one' ); ?></a>
@@ -25,6 +27,22 @@
 
 <div class="ttf-one-gallery-add-item">
 	<a href="#"><?php _e( 'Add a new gallery item', 'ttf-one' ); ?></a>
+</div>
+
+<div class="ttf-one-gallery-items">
+	<div class="ttf-one-gallery-item">
+		<div class="ttf-one-titlediv">
+			<div class="ttf-one-titlewrap">
+				<input placeholder="<?php esc_attr_e( 'Enter title here', 'ttf-one' ); ?>" type="text" name="<?php echo $section_name; ?>[title]" class="ttf-one-title ttf-one-section-header-title-input" value="" autocomplete="off" />
+			</div>
+		</div>
+
+		<div class="ttf-one-titlediv">
+			<input placeholder="<?php esc_attr_e( 'Enter link here', 'ttf-one' ); ?>" type="text" name="<?php echo $section_name; ?>[image-link]" class="ttf-one-link widefat" value="" autocomplete="off" />
+		</div>
+
+		<?php ttf_one_get_builder_base()->add_uploader( $section_name, 0 ); ?>
+	</div>
 </div>
 
 <input type="hidden" class="ttf-one-section-state" name="<?php echo $section_name; ?>[state]" value="<?php if ( isset( $ttf_one_section_data['data']['state'] ) ) echo esc_attr( $ttf_one_section_data['data']['state'] ); else echo 'open'; ?>" />
