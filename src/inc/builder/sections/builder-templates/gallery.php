e31e16d26b8caba58981e8fab0e50a4f73c58332
Zack Tollman <tollmanz@gmail.com>
2014-03-28 08:55:18 -0700

    Standardize classes in gallery template.

--- src/inc/builder/sections/builder-templates/gallery.php
+++ src/inc/builder/sections/builder-templates/gallery.php
@@ -2,7 +2,7 @@
 <?php global $ttf_one_section_data, $ttf_one_is_js_template; ?>
 <?php $section_name = ttf_one_get_section_name( $ttf_one_section_data, $ttf_one_is_js_template ); ?>
 
-<div class="ttf-one-columns-select">
+<div class="ttf-one-columns-select-wrapper">
 	<label for="ttf-one-gallery-columns"><?php _e( 'Columns', 'ttf-one' ); ?></label>
 	<select id="ttf-one-gallery-columns">
 		<option value="1">1</option>
@@ -12,7 +12,7 @@
 	</select>
 </div>
 
-<div class="ttf-one-captions-select">
+<div class="ttf-one-captions-select-wrapper">
 	<label for="ttf-one-gallery-captions"><?php _e( 'Caption Style', 'ttf-one' ); ?></label>
 	<select id="ttf-one-gallery-captions">
 		<option value="none"><?php echo esc_html( __( 'None', 'ttf-one' ) ); ?></option>
@@ -21,12 +21,12 @@
 	</select>
 </div>
 
-<div class="ttf-one-gallery-background">
-	<a href="#"><?php _e( 'Set a background image or color', 'ttf-one' ); ?></a>
+<div class="ttf-one-gallery-background-wrapper">
+	<a href="#" class="ttf-one-gallery-background"><?php _e( 'Set a background image or color', 'ttf-one' ); ?></a>
 </div>
 
-<div class="ttf-one-gallery-add-item">
-	<a href="#"><?php _e( 'Add a new gallery item', 'ttf-one' ); ?></a>
+<div class="ttf-one-gallery-add-item-wrapper">
+	<a href="#" class="ttf-one-gallery-add-item"><?php _e( 'Add a new gallery item', 'ttf-one' ); ?></a>
 </div>
 
 <div class="ttf-one-gallery-items">
