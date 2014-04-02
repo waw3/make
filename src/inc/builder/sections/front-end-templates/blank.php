44968fb0a01df1375471c7ea71359a1cbc58c142
Corey McKrill <cmckrill@jestro.com>
2014-04-01 21:32:49 -0800

    Markup tweaks for blank and gallery sections

--- src/inc/builder/sections/front-end-templates/blank.php
+++ src/inc/builder/sections/front-end-templates/blank.php
@@ -8,15 +8,15 @@ global $ttf_one_section_data;
 
 <section class="builder-section <?php echo esc_attr( ttf_one_get_builder_save()->section_classes() ); ?>">
 	<?php if ( ! empty( $ttf_one_section_data['title'] ) ) : ?>
-	<header class="section-header">
-		<h2 class="section-title">
+	<header class="builder-section-header">
+		<h2 class="builder-section-title">
 			<?php echo ttf_one_allowed_tags( $ttf_one_section_data['title'] ); ?>
 		</h2>
 	</header>
 	<?php endif; ?>
 
 	<?php if ( ! empty( $ttf_one_section_data['content'] ) ) : ?>
-	<div class="section-content">
+	<div class="builder-section-content">
 		<?php ttf_one_get_builder_save()->the_builder_content( $ttf_one_section_data['content'] ); ?>
 	</div>
 	<?php endif; ?>
