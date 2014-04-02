5112529af012f0096ac61fbd6995bdf42722fca1
Corey McKrill <cmckrill@jestro.com>
2014-04-02 14:44:54 -0800

    Builder: fix wpautop issue on builder pages
    * wpautop() was getting run twice, once when the content was written to
      post_content, the other when it was output in the page template. This
      removes the second instance by temporarily removing wpautop as a filter
      on the_content.

--- src/partials/content-page-builder.php
+++ src/partials/content-page-builder.php
@@ -6,6 +6,8 @@
 
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
 	<div class="entry-content">
+		<?php remove_filter( 'the_content', 'wpautop' ); ?>
 		<?php the_content(); ?>
+		<?php add_filter( 'the_content', 'wpautop' ); ?>
 	</div>
 </article>
