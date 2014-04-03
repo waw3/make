ab38b18970453b75d4fc4f7d4b17faa754176d86
Zack Tollman <tollmanz@gmail.com>
2014-04-03 10:06:49 -0700

    Scaffold slide template.

---
+++ src/inc/builder/sections/builder-templates/banner-slide.php
@@ -0,0 +1,15 @@
+<?php global $ttf_one_section_data, $ttf_one_is_js_template, $ttf_one_banner_id; ?>
+<?php
+$section_name = 'ttf-one-section';
+if ( true === $ttf_one_is_js_template ) {
+	$section_name .= '[{{{ parentID }}}][slides][{{{ id }}}]';
+} else {
+	$section_name .= '[' . $ttf_one_section_data['data']['id'] . '][slides][' . $ttf_one_banner_id . ']';
+}
+
+$title            = ( isset( $ttf_one_section_data['data']['slides'][ $ttf_one_banner_id ]['title'] ) ) ? $ttf_one_section_data['data']['slides'][ $ttf_one_banner_id ]['title'] : '';
+$link             = ( isset( $ttf_one_section_data['data']['slides'][ $ttf_one_banner_id ]['link'] ) ) ? $ttf_one_section_data['data']['slides'][ $ttf_one_banner_id ]['link'] : '';
+$content          = ( isset( $ttf_one_section_data['data']['slides'][ $ttf_one_banner_id ]['content'] ) ) ? $ttf_one_section_data['data']['slides'][ $ttf_one_banner_id ]['content'] : '';
+$background_color = ( isset( $ttf_one_section_data['data']['slides'][ $ttf_one_banner_id ]['background-color'] ) ) ? $ttf_one_section_data['data']['slides'][ $ttf_one_banner_id ]['background-color'] : '#ffffff';
+$alignment        = ( isset( $ttf_one_section_data['data']['slides'][ $ttf_one_banner_id ]['alignment'] ) ) ? $ttf_one_section_data['data']['slides'][ $ttf_one_banner_id ]['alignment'] : 'left';
+?>
--- src/inc/builder/sections/builder-templates/banner.php
+++ src/inc/builder/sections/builder-templates/banner.php
@@ -1,6 +1,9 @@
 <?php ttf_one_load_section_header(); ?>
-<?php global $ttf_one_section_data, $ttf_one_is_js_template; ?>
-<?php $section_name = ttf_one_get_section_name( $ttf_one_section_data, $ttf_one_is_js_template ); ?>
+<?php
+global $ttf_one_section_data, $ttf_one_is_js_template;
+$section_name = ttf_one_get_section_name( $ttf_one_section_data, $ttf_one_is_js_template );
+$section_order = ( ! empty( $ttf_one_section_data['data']['slide-order'] ) ) ? $ttf_one_section_data['data']['slide-order'] : array();
+?>
 
 <div class="ttf-one-add-slide-wrapper">
 	<a href="#" class="button button-small ttf-one-add-slide"><?php _e( 'Add Slide', 'ttf-one' ); ?></a>
@@ -17,6 +20,18 @@
 			<?php _e( 'Display navigation dots', 'ttf-one' ); ?>
 		</label>
 	</p>
+</div>
+
+<div class="ttf-one-slides">
+	<div class="ttf-one-slides-stage">
+		<?php foreach ( $section_order as $key => $section_id  ) : ?>
+			<?php if ( isset( $ttf_one_section_data['data']['slides'][ $section_id ] ) ) : ?>
+				<?php global $ttf_one_slide_id; $ttf_one_slide_id = $section_id; ?>
+				<?php get_template_part( '/inc/builder/sections/builder-templates/banner', 'slide' ); ?>
+			<?php endif; ?>
+		<?php endforeach; ?>
+	</div>
+	<input type="hidden" value="<?php echo esc_attr( implode( ',', $section_order ) ); ?>" name="<?php echo $section_name; ?>[slide-order]" class="ttf-one-slide-order" />
 </div>
 
 <input type="hidden" class="ttf-one-section-state" name="<?php echo $section_name; ?>[state]" value="<?php if ( isset( $ttf_one_section_data['data']['state'] ) ) echo esc_attr( $ttf_one_section_data['data']['state'] ); else echo 'open'; ?>" />
