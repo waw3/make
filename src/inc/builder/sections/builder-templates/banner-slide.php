8da2febcbee60d5bf91e79e6b1055c6b7d32ef05
Zack Tollman <tollmanz@gmail.com>
2014-04-03 10:25:34 -0700

    Use consistent naming convention.

--- src/inc/builder/sections/builder-templates/banner-slide.php
+++ src/inc/builder/sections/builder-templates/banner-slide.php
@@ -2,14 +2,14 @@
 <?php
 $section_name = 'ttf-one-section';
 if ( true === $ttf_one_is_js_template ) {
-	$section_name .= '[{{{ parentID }}}][slides][{{{ id }}}]';
+	$section_name .= '[{{{ parentID }}}][banner-slides][{{{ id }}}]';
 } else {
-	$section_name .= '[' . $ttf_one_section_data['data']['id'] . '][slides][' . $ttf_one_banner_id . ']';
+	$section_name .= '[' . $ttf_one_section_data['data']['id'] . '][banner-slides][' . $ttf_one_banner_id . ']';
 }
 
-$title            = ( isset( $ttf_one_section_data['data']['slides'][ $ttf_one_banner_id ]['title'] ) ) ? $ttf_one_section_data['data']['slides'][ $ttf_one_banner_id ]['title'] : '';
-$link             = ( isset( $ttf_one_section_data['data']['slides'][ $ttf_one_banner_id ]['link'] ) ) ? $ttf_one_section_data['data']['slides'][ $ttf_one_banner_id ]['link'] : '';
-$content          = ( isset( $ttf_one_section_data['data']['slides'][ $ttf_one_banner_id ]['content'] ) ) ? $ttf_one_section_data['data']['slides'][ $ttf_one_banner_id ]['content'] : '';
-$background_color = ( isset( $ttf_one_section_data['data']['slides'][ $ttf_one_banner_id ]['background-color'] ) ) ? $ttf_one_section_data['data']['slides'][ $ttf_one_banner_id ]['background-color'] : '#ffffff';
-$alignment        = ( isset( $ttf_one_section_data['data']['slides'][ $ttf_one_banner_id ]['alignment'] ) ) ? $ttf_one_section_data['data']['slides'][ $ttf_one_banner_id ]['alignment'] : 'left';
+$title            = ( isset( $ttf_one_section_data['data']['banner-slides'][ $ttf_one_banner_id ]['title'] ) ) ? $ttf_one_section_data['data']['banner-slides'][ $ttf_one_banner_id ]['title'] : '';
+$link             = ( isset( $ttf_one_section_data['data']['banner-slides'][ $ttf_one_banner_id ]['link'] ) ) ? $ttf_one_section_data['data']['banner-slides'][ $ttf_one_banner_id ]['link'] : '';
+$content          = ( isset( $ttf_one_section_data['data']['banner-slides'][ $ttf_one_banner_id ]['content'] ) ) ? $ttf_one_section_data['data']['banner-slides'][ $ttf_one_banner_id ]['content'] : '';
+$background_color = ( isset( $ttf_one_section_data['data']['banner-slides'][ $ttf_one_banner_id ]['background-color'] ) ) ? $ttf_one_section_data['data']['banner-slides'][ $ttf_one_banner_id ]['background-color'] : '#ffffff';
+$alignment        = ( isset( $ttf_one_section_data['data']['banner-slides'][ $ttf_one_banner_id ]['alignment'] ) ) ? $ttf_one_section_data['data']['banner-slides'][ $ttf_one_banner_id ]['alignment'] : 'left';
 ?>
