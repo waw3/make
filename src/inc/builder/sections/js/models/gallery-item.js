2279c6c88db0013d08f3c89937d102dc9d45f492
Zack Tollman <tollmanz@gmail.com>
2014-04-03 10:21:15 -0700

    Use correct model name.

--- src/inc/builder/sections/js/models/gallery-item.js
+++ src/inc/builder/sections/js/models/gallery-item.js
@@ -12,7 +12,7 @@ var oneApp = oneApp || {}, $oneApp = $oneApp || jQuery(oneApp);
 	});
 
 	// Set up this model as a "no URL model" where data is not synced with the server
-	oneApp.SectionModel.prototype.sync = function () { return null; };
-	oneApp.SectionModel.prototype.fetch = function () { return null; };
-	oneApp.SectionModel.prototype.save = function () { return null; };
+	oneApp.GalleryItemModel.prototype.sync = function () { return null; };
+	oneApp.GalleryItemModel.prototype.fetch = function () { return null; };
+	oneApp.GalleryItemModel.prototype.save = function () { return null; };
 })(window, Backbone, jQuery, _, oneApp, $oneApp);
