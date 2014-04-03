cdd89aa8d3552812d5a07def4d553b782283ac52
Zack Tollman <tollmanz@gmail.com>
2014-04-03 10:00:00 -0700

    Add navigation options.

--- src/inc/builder/sections/builder-templates/banner.php
+++ src/inc/builder/sections/builder-templates/banner.php
@@ -6,5 +6,18 @@
 	<a href="#" class="button button-small ttf-one-add-slide"><?php _e( 'Add Slide', 'ttf-one' ); ?></a>
 </div>
 
+<div class="ttf-one-banner-options">
+	<p>
+		<input type="checkbox" name="<?php echo $section_name; ?>[display-arrows]" value="1" />
+		<label>
+			<?php _e( 'Display navigation arrows', 'ttf-one' ); ?>
+		</label>
+		<input type="checkbox" name="<?php echo $section_name; ?>[display-dots]" value="1" />
+		<label>
+			<?php _e( 'Display navigation dots', 'ttf-one' ); ?>
+		</label>
+	</p>
+</div>
+
 <input type="hidden" class="ttf-one-section-state" name="<?php echo $section_name; ?>[state]" value="<?php if ( isset( $ttf_one_section_data['data']['state'] ) ) echo esc_attr( $ttf_one_section_data['data']['state'] ); else echo 'open'; ?>" />
 <?php ttf_one_load_section_footer(); ?>
