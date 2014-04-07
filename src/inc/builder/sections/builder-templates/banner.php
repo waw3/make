1e6a630bd97e2c4a825a1768c91e8202a63b2c80
scottrrollo <scottrrollo@gmail.com>
2014-04-08 09:09:00 +1000

    Styling Banner section.

--- src/inc/builder/sections/builder-templates/banner.php
+++ src/inc/builder/sections/builder-templates/banner.php
@@ -4,7 +4,7 @@ global $ttf_one_section_data, $ttf_one_is_js_template;
 $section_name = ttf_one_get_section_name( $ttf_one_section_data, $ttf_one_is_js_template );
 $hide_arrows   = ( ! empty( $ttf_one_section_data['data']['hide-arrows'] ) ) ? $ttf_one_section_data['data']['hide-arrows'] : 0;
 $hide_dots     = ( ! empty( $ttf_one_section_data['data']['hide-dots'] ) ) ? $ttf_one_section_data['data']['hide-dots'] : 0;
-$autoplay      = ( ! empty( $ttf_one_section_data['data']['autoplay'] ) ) ? $ttf_one_section_data['data']['autoplay'] : 0;
+$autoplay      = ( ! empty( $ttf_one_section_data['data']['autoplay'] ) ) ? $ttf_one_section_data['data']['autoplay'] : 1;
 $transition    = ( ! empty( $ttf_one_section_data['data']['transition'] ) ) ? $ttf_one_section_data['data']['transition'] : 'cross-fade';
 $delay         = ( ! empty( $ttf_one_section_data['data']['delay'] ) ) ? $ttf_one_section_data['data']['delay'] : 4000;
 $height        = ( ! empty( $ttf_one_section_data['data']['height'] ) ) ? $ttf_one_section_data['data']['height'] : 500;
@@ -12,44 +12,64 @@ $section_order = ( ! empty( $ttf_one_section_data['data']['banner-slide-order'] 
 ?>
 
 <div class="ttf-one-banner-options">
-	<input id="<?php echo $section_name; ?>[hide-arrows]" type="checkbox" name="<?php echo $section_name; ?>[hide-arrows]" value="1"<?php checked( $hide_arrows ); ?> />
-	<label for="<?php echo $section_name; ?>[hide-arrows]">
-		<?php _e( 'Hide navigation arrows', 'ttf-one' ); ?>
-	</label>
+	<div class="ttf-one-banner-options-container">
+		<h4>
+			<?php _e( 'Section height', 'ttf-one' ); ?>
+		</h4>
+		<input id="<?php echo $section_name; ?>[height]" type="text" name="<?php echo $section_name; ?>[height]" value="<?php echo absint( $height ); ?>" />
+	</div>
 
-	<input id="<?php echo $section_name; ?>[hide-dots]" type="checkbox" name="<?php echo $section_name; ?>[hide-dots]" value="1"<?php checked( $hide_dots ); ?> />
-	<label for="<?php echo $section_name; ?>[hide-dots]">
-		<?php _e( 'Hide navigation dots', 'ttf-one' ); ?>
-	</label>
+	<div class="ttf-one-banner-options-container">
+		<h4>
+			<?php _e( 'Time between slides (in ms)', 'ttf-one' ); ?>
+		</h4>
+		<input id="<?php echo $section_name; ?>[delay]" type="text" name="<?php echo $section_name; ?>[delay]" value="<?php echo absint( $delay ); ?>" />
+	</div>
 
-	<input id="<?php echo $section_name; ?>[autoplay]" type="checkbox" name="<?php echo $section_name; ?>[autoplay]" value="1"<?php checked( $autoplay ); ?> />
-	<label for="<?php echo $section_name; ?>[autoplay]">
-		<?php _e( 'Autoplay slideshow', 'ttf-one' ); ?>
-	</label>
+	<div class="ttf-one-banner-options-container">
+		<h4>
+			<?php _e( 'Transition effect', 'ttf-one' ); ?>
+		</h4>
+		<select id="<?php echo $section_name; ?>[transition]" name="<?php echo $section_name; ?>[transition]">
+			<option value="fade"<?php selected( 'cross-fade', $transition ); ?>><?php _e( 'Cross fade', 'ttf-one' ); ?></option>
+			<option value="fadeout"<?php selected( 'fade', $transition ); ?>><?php _e( 'Fade', 'ttf-one' ); ?></option>
+			<option value="scrollHorz"<?php selected( 'slide-horizontal', $transition ); ?>><?php _e( 'Slide horizontal', 'ttf-one' ); ?></option>
+			<option value="none"<?php selected( 'none', $transition ); ?>><?php echo _x( 'None', 'transition effect', 'ttf-one' ); ?></option>
+		</select>
+	</div>
 
-	<label for="<?php echo $section_name; ?>[transition]">
-		<?php _e( 'Transition effect', 'ttf-one' ); ?>
-	</label>
-	<select id="<?php echo $section_name; ?>[transition]" name="<?php echo $section_name; ?>[transition]">
-		<option value="fade"<?php selected( 'cross-fade', $transition ); ?>><?php _e( 'Cross fade', 'ttf-one' ); ?></option>
-		<option value="fadeout"<?php selected( 'fade', $transition ); ?>><?php _e( 'Fade', 'ttf-one' ); ?></option>
-		<option value="scrollHorz"<?php selected( 'slide-horizontal', $transition ); ?>><?php _e( 'Slide horizontal', 'ttf-one' ); ?></option>
-		<option value="none"<?php selected( 'none', $transition ); ?>><?php echo _x( 'None', 'transition effect', 'ttf-one' ); ?></option>
-	</select>
+	<div class="clear"></div>
 
-	<label for="<?php echo $section_name; ?>[delay]">
-		<?php _e( 'Time between slides (in ms)', 'ttf-one' ); ?>
-	</label>
-	<input id="<?php echo $section_name; ?>[delay]" type="text" name="<?php echo $section_name; ?>[delay]" value="<?php echo absint( $delay ); ?>" />
+	<h4>
+		<?php _e( 'Slideshow options', 'ttf-one' ); ?>
+	</h4>
 
-	<label for="<?php echo $section_name; ?>[height]">
-		<?php _e( 'Section height', 'ttf-one' ); ?>
-	</label>
-	<input id="<?php echo $section_name; ?>[height]" type="text" name="<?php echo $section_name; ?>[height]" value="<?php echo absint( $height ); ?>" />
+	<p>
+		<input id="<?php echo $section_name; ?>[hide-arrows]" type="checkbox" name="<?php echo $section_name; ?>[hide-arrows]" value="1"<?php checked( $hide_arrows ); ?> />
+		<label for="<?php echo $section_name; ?>[hide-arrows]">
+			<?php _e( 'Hide navigation arrows', 'ttf-one' ); ?>
+		</label>
+	</p>
+
+	<p>
+		<input id="<?php echo $section_name; ?>[hide-dots]" type="checkbox" name="<?php echo $section_name; ?>[hide-dots]" value="1"<?php checked( $hide_dots ); ?> />
+		<label for="<?php echo $section_name; ?>[hide-dots]">
+			<?php _e( 'Hide navigation dots', 'ttf-one' ); ?>
+		</label>
+	</p>
+
+	<p>
+		<input id="<?php echo $section_name; ?>[autoplay]" type="checkbox" name="<?php echo $section_name; ?>[autoplay]" value="1"<?php checked( $autoplay ); ?> />
+		<label for="<?php echo $section_name; ?>[autoplay]">
+			<?php _e( 'Autoplay slideshow', 'ttf-one' ); ?>
+		</label>
+	</p>
+
+	<div class="clear"></div>
 </div>
 
 <div class="ttf-one-add-slide-wrapper">
-	<a href="#" class="button button-small ttf-one-add-slide"><?php _e( 'Add Slide', 'ttf-one' ); ?></a>
+	<a href="#" class="button button-large ttf-one-add-slide"><?php _e( 'Add Slide', 'ttf-one' ); ?></a>
 </div>
 
 <div class="ttf-one-banner-slides">
