433e96cde418ce157a2051985029549a90f4cde7
Zack Tollman <tollmanz@gmail.com>
2014-04-07 13:12:57 -0700

    Default to showing page view.

--- src/inc/edit-page.php
+++ src/inc/edit-page.php
@@ -3,26 +3,6 @@
  * @package ttf-one
  */
 
-if ( ! function_exists( 'ttf_one_page_template_default' ) ) :
-/**
- * Change the default page template on the Add New Page screen.
- *
- * @since 1.0.0
- *
- * @return void
- */
-function ttf_one_page_template_default() {
-	global $typenow, $pagenow, $post;
-
-	// If this is a new page, set the default page template
-	if ( 'page' === $typenow && 'post-new.php' === $pagenow ) {
-		$post->page_template = 'template-builder.php';
-	}
-}
-endif;
-
-add_action( 'admin_head', 'ttf_one_page_template_default' );
-
 if ( ! function_exists( 'ttf_one_page_screen_script' ) ) :
 /**
  * Enqueue scripts that run on the Edit Page screen
