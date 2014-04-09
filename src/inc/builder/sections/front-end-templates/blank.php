e953b2712b004ad7797cc4f9b3f04b6535016519
scottrrollo <scottrrollo@gmail.com>
2014-04-10 07:08:37 +1000

    Using h3 for Blank section title.

--- src/inc/builder/sections/front-end-templates/blank.php
+++ src/inc/builder/sections/front-end-templates/blank.php
@@ -9,9 +9,9 @@ global $ttf_one_section_data;
 <section class="builder-section <?php echo esc_attr( ttf_one_get_builder_save()->section_classes( $ttf_one_section_data ) ); ?>">
 	<?php if ( ! empty( $ttf_one_section_data['title'] ) ) : ?>
 	<header class="builder-section-header">
-		<h2 class="builder-section-title">
+		<h3 class="builder-section-title">
 			<?php echo apply_filters( 'the_title', $ttf_one_section_data['title'] ); ?>
-		</h2>
+		</h3>
 	</header>
 	<?php endif; ?>
 
