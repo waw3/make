5b79fa77db812e46760bd94acbade81e6c778f0b
scottrrollo <scottrrollo@gmail.com>
2014-04-09 10:48:22 +1000

    Reworking Gallery section.

--- src/inc/builder/sections/builder-templates/gallery.php
+++ src/inc/builder/sections/builder-templates/gallery.php
@@ -9,6 +9,15 @@ $background_color = ( isset( $ttf_one_section_data['data']['background-color'] )
 $section_order    = ( ! empty( $ttf_one_section_data['data']['gallery-item-order'] ) ) ? $ttf_one_section_data['data']['gallery-item-order'] : array();
 ?>
 
+<div class="ttf-one-captions-select-wrapper">
+	<label for="<?php echo $section_name; ?>[captions]"><?php _e( 'Caption style:', 'ttf-one' ); ?></label>
+	<select id="<?php echo $section_name; ?>[captions]" name="<?php echo $section_name; ?>[captions]">
+		<option value="none"<?php selected( 'none', $captions ); ?>><?php echo esc_html( __( 'None', 'ttf-one' ) ); ?></option>
+		<option value="basic"<?php selected( 'basic', $captions ); ?>><?php echo esc_html( __( 'Basic', 'ttf-one' ) ); ?></option>
+		<option value="fancy"<?php selected( 'fancy', $captions ); ?>><?php echo esc_html( __( 'Fancy', 'ttf-one' ) ); ?></option>
+	</select>
+</div>
+
 <div class="ttf-one-columns-select-wrapper">
 	<label for="<?php echo $section_name; ?>[columns]"><?php _e( 'Columns:', 'ttf-one' ); ?></label>
 	<select id="<?php echo $section_name; ?>[columns]" name="<?php echo $section_name; ?>[columns]" class="ttf-one-gallery-columns">
@@ -19,17 +28,26 @@ $section_order    = ( ! empty( $ttf_one_section_data['data']['gallery-item-order
 	</select>
 </div>
 
-<div class="ttf-one-captions-select-wrapper">
-	<label for="<?php echo $section_name; ?>[captions]"><?php _e( 'Caption style:', 'ttf-one' ); ?></label>
-	<select id="<?php echo $section_name; ?>[captions]" name="<?php echo $section_name; ?>[captions]">
-		<option value="none"<?php selected( 'none', $captions ); ?>><?php echo esc_html( __( 'None', 'ttf-one' ) ); ?></option>
-		<option value="basic"<?php selected( 'basic', $captions ); ?>><?php echo esc_html( __( 'Basic', 'ttf-one' ) ); ?></option>
-		<option value="fancy"<?php selected( 'fancy', $captions ); ?>><?php echo esc_html( __( 'Fancy', 'ttf-one' ) ); ?></option>
-	</select>
+<div class="ttf-one-add-gallery-item-wrapper">
+	<a href="#" class="button button-primary button-large ttf-one-gallery-add-item"><?php _e( 'Add gallery item', 'ttf-one' ); ?></a>
+</div>
+
+<div class="ttf-one-gallery-items">
+	<div class="ttf-one-gallery-items-stage ttf-one-gallery-columns-<?php echo absint( $columns ); ?>">
+		<?php foreach ( $section_order as $key => $section_id  ) : ?>
+			<?php if ( isset( $ttf_one_section_data['data']['gallery-items'][ $section_id ] ) ) : ?>
+				<?php global $ttf_one_gallery_id; $ttf_one_gallery_id = $section_id; ?>
+				<?php get_template_part( '/inc/builder/sections/builder-templates/gallery', 'item' ); ?>
+			<?php endif; ?>
+		<?php endforeach; ?>
+	</div>
+	<input type="hidden" value="<?php echo esc_attr( implode( ',', $section_order ) ); ?>" name="<?php echo $section_name; ?>[gallery-item-order]" class="ttf-one-gallery-item-order" />
 </div>
 
 <div class="clear"></div>
 
+
+<div class="ttf-one-gallery-background-options-container">
 <div class="ttf-one-gallery-background-image-wrapper">
 	<?php
 		ttf_one_get_builder_base()->add_uploader(
@@ -57,24 +75,9 @@ $section_order    = ( ! empty( $ttf_one_section_data['data']['gallery-item-order
 		<?php _e( 'Darken to improve readability', 'ttf-one' ); ?>
 	</label>
 </div>
+</div>
 
 <div class="clear"></div>
-
-<div class="ttf-one-add-gallery-item-wrapper">
-	<a href="#" class="button button-primary button-large ttf-one-gallery-add-item"><?php _e( 'Add gallery item', 'ttf-one' ); ?></a>
-</div>
-
-<div class="ttf-one-gallery-items">
-	<div class="ttf-one-gallery-items-stage ttf-one-gallery-columns-<?php echo absint( $columns ); ?>">
-		<?php foreach ( $section_order as $key => $section_id  ) : ?>
-			<?php if ( isset( $ttf_one_section_data['data']['gallery-items'][ $section_id ] ) ) : ?>
-				<?php global $ttf_one_gallery_id; $ttf_one_gallery_id = $section_id; ?>
-				<?php get_template_part( '/inc/builder/sections/builder-templates/gallery', 'item' ); ?>
-			<?php endif; ?>
-		<?php endforeach; ?>
-	</div>
-	<input type="hidden" value="<?php echo esc_attr( implode( ',', $section_order ) ); ?>" name="<?php echo $section_name; ?>[gallery-item-order]" class="ttf-one-gallery-item-order" />
-</div>
 
 <input type="hidden" class="ttf-one-section-state" name="<?php echo $section_name; ?>[state]" value="<?php if ( isset( $ttf_one_section_data['data']['state'] ) ) echo esc_attr( $ttf_one_section_data['data']['state'] ); else echo 'open'; ?>" />
 <?php ttf_one_load_section_footer(); ?>
