6469310b04788c378d97525107bb04eec19da878
Corey McKrill <cmckrill@jestro.com>
2014-04-10 16:54:18 -0800

    Implement featured image view options

--- src/partials/entry-thumbnail.php
+++ src/partials/entry-thumbnail.php
@@ -3,10 +3,16 @@
  * @package ttf-one
  */
 
-$thumbnail_size = ( is_singular() ) ? 'large' : 'thumbnail';
+$thumb_key = 'layout-' . ttf_one_get_view() . '-featured-images';
+$thumb_option = ttf_one_sanitize_choice( get_theme_mod( $thumb_key, ttf_one_get_default( $thumb_key ) ), $thumb_key );
+if ( 'post-header' === $thumb_option ) :
+	$thumbnail_size = 'large';
+elseif ( 'thumbnail' === $thumb_option ) :
+	$thumbnail_size = ( is_singular() ) ? 'medium' : 'thumbnail';
+endif;
 ?>
 
-<?php if ( $thumbnail_id = get_post_thumbnail_id() ) : ?>
+<?php if ( 'none' !== $thumb_option && $thumbnail_id = get_post_thumbnail_id() ) : ?>
 <figure class="entry-thumbnail">
 	<?php the_post_thumbnail( $thumbnail_size ); ?>
 	<?php if ( is_singular() && has_excerpt( $thumbnail_id ) ) : ?>
