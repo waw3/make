d76590552b2ff59d95da18468f7eb20f6e1b6b34
Corey McKrill <cmckrill@jestro.com>
2014-04-15 12:12:44 -0800

    Move comments logic to partial template

--- src/attachment.php
+++ src/attachment.php
@@ -15,12 +15,7 @@ get_header();
 
 		<?php get_template_part( 'partials/content', 'attachment' ); ?>
 
-		<?php
-		// If comments are open or we have at least one comment, load up the comment template
-		if ( comments_open() || '0' != get_comments_number() ) :
-			comments_template();
-		endif;
-		?>
+		<?php get_template_part( 'partials/content', 'comments' ); ?>
 
 	<?php endwhile; ?>
 
--- src/template-builder.php
+++ src/template-builder.php
@@ -17,12 +17,7 @@ get_header();
 
 		<?php get_template_part( 'partials/content', 'page-builder' ); ?>
 
-		<?php
-		// If comments are open or we have at least one comment, load up the comment template
-		if ( comments_open() || '0' != get_comments_number() ) :
-			comments_template();
-		endif;
-		?>
+		<?php get_template_part( 'partials/content', 'comments' ); ?>
 
 	<?php endwhile; ?>
 
