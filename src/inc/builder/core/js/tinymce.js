4493686d4e56057becbeb1634824810e286f619e
Zack Tollman <tollmanz@gmail.com>
2014-04-21 14:34:15 -0700

    Make wpActiveEditor available to IEFE.

--- src/inc/builder/core/js/tinymce.js
+++ src/inc/builder/core/js/tinymce.js
@@ -1,7 +1,7 @@
-/*global jQuery, tinyMCEPreInit, tinyMCE, QTags, quicktags, ttfOneMCE */
+/*global jQuery, tinyMCEPreInit, tinyMCE, QTags, quicktags, wpActiveEditor, ttfOneMCE */
 var oneApp = oneApp || {}, $oneApp = $oneApp || jQuery(oneApp);
 
-(function ($, oneApp, tinyMCEPreInit, tinyMCE, QTags, quicktags, ttfOneMCE) {
+(function ($, oneApp, tinyMCEPreInit, tinyMCE, QTags, quicktags, wpActiveEditor, ttfOneMCE) {
 	'use strict';
 
 	// Initiate a new TinyMCE editor instance
@@ -107,4 +107,4 @@ var oneApp = oneApp || {}, $oneApp = $oneApp || jQuery(oneApp);
 		this.syncEditorHeight(evt, baseEl);
 	};
 
-})(jQuery, oneApp, tinyMCEPreInit, tinyMCE, QTags, quicktags, ttfOneMCE);
+})(jQuery, oneApp, tinyMCEPreInit, tinyMCE, QTags, quicktags, wpActiveEditor, ttfOneMCE);
