19f6dc979ac07386b9927b6b7a72a57c948ea32e
Zack Tollman <tollmanz@gmail.com>
2014-04-21 14:23:59 -0700

    Fix jsHint issues in global.js.

--- src/js/global.js
+++ src/js/global.js
@@ -93,10 +93,11 @@
 		skipLinkFocusFix: function() {
 			var is_webkit = navigator.userAgent.toLowerCase().indexOf( 'webkit' ) > -1,
 				is_opera  = navigator.userAgent.toLowerCase().indexOf( 'opera' )  > -1,
-				is_ie     = navigator.userAgent.toLowerCase().indexOf( 'msie' )   > -1;
+				is_ie     = navigator.userAgent.toLowerCase().indexOf( 'msie' )   > -1,
+				eventMethod;
 
 			if ( ( is_webkit || is_opera || is_ie ) && 'undefined' !== typeof( document.getElementById ) ) {
-				var eventMethod = ( window.addEventListener ) ? 'addEventListener' : 'attachEvent';
+				eventMethod = ( window.addEventListener ) ? 'addEventListener' : 'attachEvent';
 				window[ eventMethod ]( 'hashchange', function() {
 					var element = document.getElementById( location.hash.substring( 1 ) );
 
