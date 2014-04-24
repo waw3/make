a22336c97b4172887db90ec3e642bbdbd09a60ef
Zack Tollman <tollmanz@gmail.com>
2014-04-24 14:27:58 -0700

    Add missing footer template.

---
+++ src/sidebar-footer-4.php
@@ -0,0 +1,8 @@
+<?php
+/**
+ * @package ttf-one
+ */
+?>
+<section id="footer-4" class="widget-area <?php echo ( is_active_sidebar( 'footer-4' ) ) ? 'active' : 'inactive'; ?>" role="complementary">
+	<?php dynamic_sidebar( 'footer-4' ); ?>
+</section>
