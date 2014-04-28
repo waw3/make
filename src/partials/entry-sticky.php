a4c7decd5f5872ab5a11805ea089884b3a78c187
Zack Tollman <tollmanz@gmail.com>
2014-04-28 14:21:03 -0500

    Escape sticky label.

--- src/partials/entry-sticky.php
+++ src/partials/entry-sticky.php
@@ -6,6 +6,6 @@
 
 <?php if ( is_sticky() && $sticky_label = get_theme_mod( 'general-sticky-label', ttf_one_get_default( 'general-sticky-label' ) ) ) : ?>
 	<span class="sticky-post-label">
-		<?php echo $sticky_label; ?>
+		<?php echo esc_html( wp_strip_all_tags( $sticky_label ) ); ?>
 	</span>
-<?php endif; ?>
+<?php endif;
