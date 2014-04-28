dc20cfd9a0f5f409403aec61cf4ffba60495085f
Zack Tollman <tollmanz@gmail.com>
2014-04-28 14:51:31 -0500

    Improve nav link performance.

--- src/partials/nav-post.php
+++ src/partials/nav-post.php
@@ -2,22 +2,25 @@
 /**
  * @package ttf-one
  */
-?>
 
-<?php if ( get_adjacent_post( false, '', true ) || get_adjacent_post( false, '', false ) ) : ?>
+$next_link = get_next_post_link(
+	'<div class="nav-next">%link</div>',
+	'%title'
+);
+
+$previous_link = get_previous_post_link(
+	'<div class="nav-previous">%link</div>',
+	'%title'
+);
+
+if ( '' !== $next_link || '' !== $previous_link ) : ?>
 <nav class="navigation post-navigation" role="navigation">
 	<span class="screen-reader-text"><?php _e( 'Post navigation', 'ttf-one' ); ?></span>
 	<div class="nav-links">
-	<?php
-	previous_post_link(
-		'<div class="nav-previous">%link</div>',
-		'%title'
-	);
-	next_post_link(
-		'<div class="nav-next">%link</div>',
-		'%title'
-	);
-	?>
+		<?php
+		echo $previous_link;
+		echo $next_link;
+		?>
 	</div>
 </nav>
-<?php endif; ?>
+<?php endif;
