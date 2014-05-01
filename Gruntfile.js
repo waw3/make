d5ee236cc920bd25e105942f7d836a0b6fa49be6
Zack Tollman <tollmanz@gmail.com>
2014-04-30 21:17:48 -0500

    Bump SASS and set correct config.

--- Gruntfile.js
+++ Gruntfile.js
@@ -9,7 +9,7 @@ module.exports = function( grunt ) {
 		sass: {
 			theme: {
 				options: {
-					outputStyle: 'expanded'
+					outputStyle: 'nested'
 				},
 				files: {
 					'src/style.css': 'src/assets/sass/style.scss'
