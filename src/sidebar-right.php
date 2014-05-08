1288d9ec40bf8016fd05a070bfb61968f1b26f23
Corey McKrill <cmckrill@jestro.com>
2014-05-07 18:21:26 -0800

    Adjust sidebar ids and classes
    Rather than altering the container id when the sidebar id is filtered,
    add the sidebar id as another class. This way, styles that are specific
    to the location of the sidebar will still be applied even if a non-default
    sidebar is used in that location, as long as the styles use the id.

--- src/sidebar-right.php
+++ src/sidebar-right.php
@@ -5,7 +5,7 @@
 
 $sidebar_id = esc_attr( apply_filters( 'ttfmake_sidebar_right', 'sidebar-right' ) );
 ?>
-<section id="<?php echo $sidebar_id; ?>" class="widget-area <?php echo ( is_active_sidebar( $sidebar_id ) ) ? 'active' : 'inactive'; ?>" role="complementary">
+<section id="sidebar-right" class="widget-area <?php echo $sidebar_id; ?> <?php echo ( is_active_sidebar( $sidebar_id ) ) ? 'active' : 'inactive'; ?>" role="complementary">
 	<?php if ( ! dynamic_sidebar( $sidebar_id ) ) : ?>
 		&nbsp;
 	<?php endif; ?>
