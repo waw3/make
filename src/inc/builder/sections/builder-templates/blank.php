6c25ebd755e70547d504c365e26d25e51fb3d9dd
Corey McKrill <cmckrill@jestro.com>
2014-05-09 15:03:01 -0800

    Section defaults: refactor Blank section

--- src/inc/builder/sections/builder-templates/blank.php
+++ src/inc/builder/sections/builder-templates/blank.php
@@ -7,11 +7,17 @@ ttfmake_load_section_header();
 
 global $ttfmake_section_data, $ttfmake_is_js_template;
 $section_name = ttfmake_get_section_name( $ttfmake_section_data, $ttfmake_is_js_template );
+
+$keys = array(
+	'title',
+	'content',
+);
+$data = ttfmake_parse_section_data( $ttfmake_section_data['data'], $keys, 'blank' );
 ?>
 
 	<div class="ttfmake-titlediv">
 		<div class="ttfmake-titlewrap">
-			<input placeholder="<?php esc_attr_e( 'Enter title here' ); ?>" type="text" name="<?php echo $section_name; ?>[title]" class="ttfmake-title ttfmake-section-header-title-input" value="<?php if ( isset( $ttfmake_section_data['data']['title'] ) ) echo esc_attr( htmlspecialchars( $ttfmake_section_data['data']['title'] ) ); ?>" autocomplete="off" />
+			<input placeholder="<?php esc_attr_e( 'Enter title here' ); ?>" type="text" name="<?php echo $section_name; ?>[title]" class="ttfmake-title ttfmake-section-header-title-input" value="<?php echo esc_attr( htmlspecialchars( $data['title'] ) ); ?>" autocomplete="off" />
 		</div>
 	</div>
 
@@ -26,9 +32,8 @@ $section_name = ttfmake_get_section_name( $ttfmake_section_data, $ttfmake_is_js_
 	if ( true === $ttfmake_is_js_template ) : ?>
 		<?php ttfmake_get_builder_base()->wp_editor( '', ttfmake_get_wp_editor_id( $ttfmake_section_data, $ttfmake_is_js_template ), $editor_settings ); ?>
 	<?php else : ?>
-		<?php $content = ( isset( $ttfmake_section_data['data']['content'] ) ) ? $ttfmake_section_data['data']['content'] : ''; ?>
-		<?php ttfmake_get_builder_base()->wp_editor( $content, ttfmake_get_wp_editor_id( $ttfmake_section_data, $ttfmake_is_js_template ), $editor_settings ); ?>
+		<?php ttfmake_get_builder_base()->wp_editor( $data['content'], ttfmake_get_wp_editor_id( $ttfmake_section_data, $ttfmake_is_js_template ), $editor_settings ); ?>
 	<?php endif; ?>
 
-	<input type="hidden" class="ttfmake-section-state" name="<?php echo $section_name; ?>[state]" value="<?php if ( isset( $ttfmake_section_data['data']['state'] ) ) echo esc_attr( $ttfmake_section_data['data']['state'] ); else echo 'open'; ?>" />
+	<input type="hidden" class="ttfmake-section-state" name="<?php echo $section_name; ?>[state]" value="<?php if ( isset( $data['state'] ) ) echo esc_attr( $data['state'] ); else echo 'open'; ?>" />
 <?php ttfmake_load_section_footer();
