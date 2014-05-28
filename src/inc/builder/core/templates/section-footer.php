e26e4b5c2f27fc79a47fe06b1775d2214d4988e7
Zack Tollman <tollmanz@gmail.com>
2014-05-28 14:27:01 -0700

    Make action links filterable.
    Allow links in the footer of the builder sections to be filterable.
    This allows for adding additional links in the footer area.

--- src/inc/builder/core/templates/section-footer.php
+++ src/inc/builder/core/templates/section-footer.php
@@ -2,11 +2,26 @@
 /**
  * @package Make
  */
-?>
 
-		<a href="#" class="ttfmake-section-remove">
-			<?php _e( 'Remove this section', 'make' ); ?>
+$links = apply_filters( 'ttfmake_builder_section_footer_links', array(
+	100 => array(
+		'href'  => '#',
+		'class' => 'ttfmake-section-remove',
+		'label' => __( 'Remove this section', 'make' )
+	)
+) );
+?>
+		<?php foreach ( $links as $link ) : ?>
+		<?php
+			$href  = ( isset( $link['href'] ) ) ? ' href="' . esc_url( $link['href'] ) . '"' : '';
+			$id    = ( isset( $link['id'] ) ) ? ' id="' . esc_attr( $link['id'] ) . '"' : '';
+			$class = ( isset( $link['class'] ) ) ? ' class="' . esc_attr( $link['class'] ) . '"' : '';
+			$label = ( isset( $link['label'] ) ) ? esc_html( $link['label'] ) : '';
+		?>
+		<a<?php echo $href . $id . $class; ?>>
+			<?php echo $label; ?>
 		</a>
+		<?php endforeach; ?>
 	</div>
 <?php if ( ! isset( $ttfmake_is_js_template ) || true !== $ttfmake_is_js_template ) : ?>
 </div>
