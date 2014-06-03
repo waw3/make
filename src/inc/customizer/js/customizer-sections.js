88762495d81a685a51e385f97ac0dc851ae96b66
Corey McKrill <cmckrill@jestro.com>
2014-06-03 14:07:21 -0800

    Add Make Plus notice to Customizer header

--- src/inc/customizer/js/customizer-sections.js
+++ src/inc/customizer/js/customizer-sections.js
@@ -2,8 +2,10 @@
  * @package Make
  */
 
+/* global jQuery, ttfmakeCustomizerL10n */
 ( function( $ ) {
-	var api = wp.customize;
+	var api = wp.customize,
+		upgrade;
 
 	/**
 	 * Visibility toggling for some controls
@@ -54,4 +56,13 @@
 			var val = $(this).val();
 			return !isNaN(parseFloat(+val)) && isFinite(val);
 		}).attr('disabled', 'disabled');
+
+	// Add Make Plus message
+	if ('undefined' !== typeof ttfmakeCustomizerL10n) {
+		upgrade = $('<a class="ttfmake-customize-plus"></a>')
+			.attr('href', ttfmakeCustomizerL10n.plusURL)
+			.attr('target', '_blank')
+			.text(ttfmakeCustomizerL10n.plusLabel);
+		$('.preview-notice').append(upgrade);
+	}
 } )( jQuery );
