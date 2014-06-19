e1cfa64a1524e6f78ddd847db9a24db7b01bdced
Zack Tollman <tollmanz@gmail.com>
2014-06-19 08:39:32 -0700

    Add markup for comments number icons.

--- src/partials/entry-comment-count.php
+++ src/partials/entry-comment-count.php
@@ -10,7 +10,13 @@ $comment_count_option = ttfmake_sanitize_choice( get_theme_mod( $comment_count_k
 <?php if ( 'none' !== $comment_count_option ) : ?>
 	<div class="entry-comment-count">
 	<?php if ( 'icon' === $comment_count_option ) : ?>
-		<?php comments_number(); ?>
+		<?php
+		comments_number(
+			'<span class="comment-count-icon zero">0</span>',
+			'<span class="comment-count-icon one">1</span>',
+			'<span class="comment-count-icon many">%</span>'
+		);
+		?>
 	<?php else : ?>
 		<?php comments_number(); ?>
 	<?php endif; ?>
