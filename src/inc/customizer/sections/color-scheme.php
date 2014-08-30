ea617222f0b28716964ec40030affc07eb7b0557
Corey McKrill <cmckrill@jestro.com>
2014-08-30 13:31:57 -0800

    Fill out existing color scheme options

--- src/inc/customizer/sections/color-scheme.php
+++ src/inc/customizer/sections/color-scheme.php
@@ -19,31 +19,177 @@ function ttfmake_customizer_define_colorscheme_sections( $sections ) {
 	/**
 	 * General
 	 */
+	$colorscheme_sections['color-general'] = array(
+		'panel' => $panel,
+		'title' => __( 'General', 'make' ),
+		'options' => array(
+			'color-primary' => array(
+				'setting' => array(
+					'sanitize_callback'	=> 'maybe_hash_hex_color',
+				),
+				'control' => array(
+					'control_type'		=> 'WP_Customize_Color_Control',
+					'label'				=> __( 'Primary Color', 'make' ),
+				),
+			),
+			'color-secondary' => array(
+				'setting' => array(
+					'sanitize_callback'	=> 'maybe_hash_hex_color',
+				),
+				'control' => array(
+					'control_type'		=> 'WP_Customize_Color_Control',
+					'label'				=> __( 'Secondary Color', 'make' ),
+				),
+			),
+			'color-text' => array(
+				'setting' => array(
+					'sanitize_callback'	=> 'maybe_hash_hex_color',
+				),
+				'control' => array(
+					'control_type'		=> 'WP_Customize_Color_Control',
+					'label'				=> __( 'Text Color', 'make' ),
+				),
+			),
+			'color-detail' => array(
+				'setting' => array(
+					'sanitize_callback'	=> 'maybe_hash_hex_color',
+				),
+				'control' => array(
+					'control_type'		=> 'WP_Customize_Color_Control',
+					'label'				=> __( 'Detail Color', 'make' ),
+				),
+			),
+		),
+	);
 
 	/**
 	 * Background
 	 */
+	$colorscheme_sections['color-background'] = array(
+		'panel' => $panel,
+		'title' => __( 'Background', 'make' ),
+		'options' => array(
+			'main-background-color' => array(
+				'setting' => array(
+					'sanitize_callback'	=> 'maybe_hash_hex_color',
+				),
+				'control' => array(
+					'control_type'		=> 'WP_Customize_Color_Control',
+					'label'				=> __( 'Main Column Background Color', 'make' ),
+				),
+			),
+		),
+	);
 
 	/**
 	 * Header
 	 */
-
-	/**
-	 * Main Column
-	 */
+	$colorscheme_sections['color-header'] = array(
+		'panel' => $panel,
+		'title' => __( 'Header', 'make' ),
+		'options' => array(
+			'header-bar-background-color' => array(
+				'setting' => array(
+					'sanitize_callback'	=> 'maybe_hash_hex_color',
+				),
+				'control' => array(
+					'control_type'		=> 'WP_Customize_Color_Control',
+					'label'				=> __( 'Header Bar Background Color', 'make' ),
+				),
+			),
+			'header-bar-text-color' => array(
+				'setting' => array(
+					'sanitize_callback'	=> 'maybe_hash_hex_color',
+				),
+				'control' => array(
+					'control_type'		=> 'WP_Customize_Color_Control',
+					'label'				=> __( 'Header Bar Text Color', 'make' ),
+				),
+			),
+			'header-bar-border-color' => array(
+				'setting' => array(
+					'sanitize_callback'	=> 'maybe_hash_hex_color',
+				),
+				'control' => array(
+					'control_type'		=> 'WP_Customize_Color_Control',
+					'label'				=> __( 'Header Bar Border Color', 'make' ),
+				),
+			),
+			'header-color-line' => array(
+				'control' => array(
+					'control_type'		=> 'TTFMAKE_Customize_Misc_Control',
+					'type'				=> 'line',
+				),
+			),
+			'header-background-color' => array(
+				'setting' => array(
+					'sanitize_callback'	=> 'maybe_hash_hex_color',
+				),
+				'control' => array(
+					'control_type'		=> 'WP_Customize_Color_Control',
+					'label'				=> __( 'Header Background Color', 'make' ),
+				),
+			),
+			'header-text-color' => array(
+				'setting' => array(
+					'sanitize_callback'	=> 'maybe_hash_hex_color',
+				),
+				'control' => array(
+					'control_type'		=> 'WP_Customize_Color_Control',
+					'label'				=> __( 'Header Text Color', 'make' ),
+				),
+			),
+		),
+	);
 
 	/**
 	 * Sidebars
+	 *
+	 * TODO
 	 */
 
 	/**
 	 * Widgets
+	 *
+	 * TODO
 	 */
 
 	/**
 	 * Footer
 	 */
-
+	$colorscheme_sections['color-footer'] = array(
+		'panel' => $panel,
+		'title' => __( 'Footer', 'make' ),
+		'options' => array(
+			'footer-background-color' => array(
+				'setting' => array(
+					'sanitize_callback'	=> 'maybe_hash_hex_color',
+				),
+				'control' => array(
+					'control_type'		=> 'WP_Customize_Color_Control',
+					'label'				=> __( 'Footer Background Color', 'make' ),
+				),
+			),
+			'footer-text-color' => array(
+				'setting' => array(
+					'sanitize_callback'	=> 'maybe_hash_hex_color',
+				),
+				'control' => array(
+					'control_type'		=> 'WP_Customize_Color_Control',
+					'label'				=> __( 'Footer Text Color', 'make' ),
+				),
+			),
+			'footer-border-color' => array(
+				'setting' => array(
+					'sanitize_callback'	=> 'maybe_hash_hex_color',
+				),
+				'control' => array(
+					'control_type'		=> 'WP_Customize_Color_Control',
+					'label'				=> __( 'Footer Border Color', 'make' ),
+				),
+			),
+		),
+	);
 
 	// Filter the definitions
 	$colorscheme_sections = apply_filters( 'make_customizer_colorscheme_sections', $colorscheme_sections );
