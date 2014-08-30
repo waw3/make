b9dbce85a1c1e220de38e3bc1975a66e47e07249
Corey McKrill <cmckrill@jestro.com>
2014-08-29 22:08:35 -0800

    Add compatibility migration function for header font options

--- src/inc/customizer/compatibility.php
+++ src/inc/customizer/compatibility.php
@@ -8,7 +8,7 @@ function ttfmake_customizer_get_key_conversions() {
 	// $new => $old
 	$conversions = array(
 		'font-site-title-family' => 'font-site-title',
-		'font-header-family'     => 'font-header',
+		'font-h1-family'		 => 'font-header',
 		'font-body-family'       => 'font-body',
 	);
 
@@ -49,3 +49,61 @@ function ttfmake_customizer_add_key_conversion_filters() {
 }
 
 add_action( 'wp', 'ttfmake_customizer_add_key_conversion_filters' );
+
+
+function ttfmake_customizer_migrate_header_options() {
+	$theme_mods = get_theme_mods();
+
+	// New header option keys
+	$new_header_family_keys = array(
+		'font-h1-family',
+		'font-h2-family',
+		'font-h3-family',
+		'font-h4-family',
+		'font-h5-family',
+		'font-h6-family',
+	);
+	$new_header_size_keys = array(
+		'font-h1-size',
+		'font-h2-size',
+		'font-h3-size',
+		'font-h4-size',
+		'font-h5-size',
+		'font-h6-size',
+	);
+	$new_header_keys = array_merge( $new_header_family_keys, $new_header_size_keys );
+
+	// Test for new header keys in the theme mod array
+	$diff = array_diff_key( $new_header_keys, $theme_mods );
+	$has_new_keys = ( count( $new_header_keys ) !== count( $diff ) );
+
+	// Only set new theme mod values if no new keys exist and one of the old keys has a non-default value
+	if ( ! $has_new_keys ) {
+		// Check for customized header font family
+		if ( isset( $theme_mods['font-header'] ) ) {
+			$header_family = get_theme_mod( 'font-header' );
+			foreach ( $new_header_family_keys as $key ) {
+				set_theme_mod( $key, $header_family );
+			}
+			remove_theme_mod( 'font-header' );
+		}
+
+		// Check for customized header font size
+		if ( isset( $theme_mods['font-header-size'] ) ) {
+			$header_size = get_theme_mod( 'font-header-size' );
+			$percent = apply_filters( 'ttfmake_font_relative_size', array(
+				'h1' => 100,
+				'h2' => 68,
+				'h3' => 48,
+				'h4' => 48,
+				'h5' => 32,
+				'h6' => 28,
+			) );
+			foreach ( $new_header_size_keys as $key ) {
+				$h = preg_replace( '/font-(h\d)-size/', '$1', $key );
+				set_theme_mod( $key, ttfmake_get_relative_font_size( $header_size, $percent[$h] ) );
+			}
+			remove_theme_mod( 'font-header-size' );
+		}
+	}
+}
