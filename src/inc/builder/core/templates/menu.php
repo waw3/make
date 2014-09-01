729d4c29ba4685cefd91158f2377d092d1104603
scottrrollo <scottrrollo@gmail.com>
2014-09-02 08:14:02 +1000

    Removing section descriptions from builder menu.

--- src/inc/builder/core/templates/menu.php
+++ src/inc/builder/core/templates/menu.php
@@ -11,7 +11,7 @@ $class = ( 'c' === get_user_setting( 'ttfmakemt' . get_the_ID() ) ) ? 'closed' :
 		<ul class="ttfmake-menu-list">
 			<?php foreach ( ttfmake_get_sections_by_order() as $key => $item ) : ?>
 				<li class="ttfmake-menu-list-item">
-					<a href="#" title="<?php esc_attr_e( 'Add', 'make' ); ?>" class="ttfmake-menu-list-item-link" id="ttfmake-menu-list-item-link-<?php echo esc_attr( $item['id'] ); ?>" data-section="<?php echo esc_attr( $item['id'] ); ?>">
+					<a href="#" title="<?php echo esc_html( $item['description'] ); ?>" class="ttfmake-menu-list-item-link" id="ttfmake-menu-list-item-link-<?php echo esc_attr( $item['id'] ); ?>" data-section="<?php echo esc_attr( $item['id'] ); ?>">
 						<div class="ttfmake-menu-list-item-link-icon-wrapper clear">
 							<span class="ttfmake-menu-list-item-link-icon"></span>
 						</div>
@@ -19,9 +19,6 @@ $class = ( 'c' === get_user_setting( 'ttfmakemt' . get_the_ID() ) ) ? 'closed' :
 							<h4>
 								<?php echo esc_html( $item['label'] ); ?>
 							</h4>
-							<p>
-								<?php echo esc_html( $item['description'] ); ?>
-							</p>
 						</div>
 					</a>
 				</li>
