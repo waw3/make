7da986bbcc0ada8829457b265e144ce6fd65d99b
Zack Tollman <tollmanz@gmail.com>
2014-09-04 11:30:44 -0700

    Use correct name for columns number.

--- src/inc/builder/sections/section-definitions.php
+++ src/inc/builder/sections/section-definitions.php
@@ -83,7 +83,7 @@ class TTFMAKE_Section_Definitions {
 				),
 				200 => array(
 					'type'    => 'select',
-					'name'    => 'columns',
+					'name'    => 'columns-number',
 					'label'   => __( 'Columns', 'make' ),
 					'default' => 3,
 					'options' => array(
