5d2f007c15321c703241592c95361769119cb457
Zack Tollman <tollmanz@gmail.com>
2014-09-08 09:29:07 -0700

    Hook up edit content button.

--- src/inc/builder/sections/builder-templates/text.php
+++ src/inc/builder/sections/builder-templates/text.php
@@ -61,7 +61,7 @@ $columns_class  = ( in_array( $columns_number, range( 1, 4 ) ) && true !== $ttfm
 			<div class="ttfmake-titlewrap">
 				<input placeholder="<?php esc_attr_e( 'Enter title here', 'make' ); ?>" type="text" name="<?php echo $column_name; ?>[title]" class="ttfmake-title ttfmake-section-header-title-input" value="<?php echo esc_attr( htmlspecialchars( $title ) ); ?>" autocomplete="off" />
 
-				<a href="#" class="edit-text-column-link" title="<?php echo esc_attr( __( 'Edit content', 'make' ) ); ?>">
+				<a href="#" class="edit-content-link edit-text-column-link" title="<?php echo esc_attr( __( 'Edit content', 'make' ) ); ?>" data-textarea="<?php echo esc_attr( $textarea_id ); ?>" data-iframe="<?php echo esc_attr( $iframe_id ); ?>">
 					<span>
 						<?php _e( 'Edit text column', 'make' ); ?>
 					</span>
