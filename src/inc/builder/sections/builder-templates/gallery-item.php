9566d687655cdc0d4bda38c72ca6c1dd6199b7dc
scottrrollo <scottrrollo@gmail.com>
2014-09-10 08:10:25 +1000

    Styling placeholder. Styling sortable handle.

--- src/inc/builder/sections/builder-templates/gallery-item.php
+++ src/inc/builder/sections/builder-templates/gallery-item.php
@@ -16,13 +16,15 @@ $link        = ( isset( $ttfmake_section_data['data']['gallery-items'][ $ttfmake
 $image_id    = ( isset( $ttfmake_section_data['data']['gallery-items'][ $ttfmake_gallery_id ]['image-id'] ) ) ? $ttfmake_section_data['data']['gallery-items'][ $ttfmake_gallery_id ]['image-id'] : 0;
 $description = ( isset( $ttfmake_section_data['data']['gallery-items'][ $ttfmake_gallery_id ]['description'] ) ) ? $ttfmake_section_data['data']['gallery-items'][ $ttfmake_gallery_id ]['description'] : '';
 ?>
+
 <?php if ( true !== $ttfmake_is_js_template ) : ?>
 <div class="ttfmake-gallery-item" id="ttfmake-gallery-item-<?php echo esc_attr( $ttfmake_gallery_id ); ?>" data-id="<?php echo esc_attr( $ttfmake_gallery_id ); ?>" data-section-type="gallery-item">
-<?php endif; ?>
+
 	<div title="<?php esc_attr_e( 'Drag-and-drop this column into place', 'make' ); ?>" class="ttfmake-sortable-handle">
 		<div class="sortable-background"></div>
 	</div>
 
+<?php endif; ?>
 	<?php echo ttfmake_get_builder_base()->add_uploader( $section_name, ttfmake_sanitize_image_id( $image_id ) ); ?>
 
 	<a href="#" class="edit-gallery-item-link">
