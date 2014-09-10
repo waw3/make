5e1f97e4c9673fdb123f7380ce933e82de4587ed
Zack Tollman <tollmanz@gmail.com>
2014-09-10 10:06:28 -0700

    Add proper label for edit content overlay.

--- src/inc/builder/core/templates/overlay-header.php
+++ src/inc/builder/core/templates/overlay-header.php
@@ -1,5 +1,8 @@
-<?php global $ttfmake_overlay_id; ?>
-<?php if ( 'ttfmake-tinymce-overlay' === $ttfmake_overlay_id ) : ?>
+<?php
+global $ttfmake_overlay_id;
+$type = ( 'ttfmake-tinymce-overlay' === $ttfmake_overlay_id ) ? 'mce' : 'config';
+?>
+<?php if ( 'mce' === $type ) : ?>
 <div class="ttfmake-overlay" id="<?php echo $ttfmake_overlay_id; ?>">
 <?php else : ?>
 <div class="ttfmake-overlay <?php echo $ttfmake_overlay_id; ?>">
@@ -7,7 +10,7 @@
 	<div class="ttfmake-overlay-wrapper">
 		<div class="ttfmake-overlay-header">
 			<div class="ttfmake-overlay-window-head">
-				<div class="ttfmake-overlay-title">Configure Section</div>
+				<div class="ttfmake-overlay-title"><?php ( 'config' === $type ) ? _e( 'Configure Section', 'make' ) : _e( 'Edit Content', 'make' ); ?></div>
 				<span class="ttfmake-overlay-close" aria-hidden="true">&#215;</span>
 			</div>
 		</div>
