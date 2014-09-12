5413c2f60c37517718c4e3feb028554ffc4a1578
Zack Tollman <tollmanz@gmail.com>
2014-09-12 11:44:54 -0700

    Do not autoresize Make TinyMCE.

--- src/inc/builder/core/templates/overlay-tinymce.php
+++ src/inc/builder/core/templates/overlay-tinymce.php
@@ -4,10 +4,12 @@ $ttfmake_overlay_id = 'ttfmake-tinymce-overlay';
 
 get_template_part( '/inc/builder/core/templates/overlay', 'header' );
 
-wp_editor(
-	'',
-	'make',
-	array()
-);
+wp_editor( '', 'make', array(
+	'tinymce'       => array(
+		'wp_autoresize_on' => false,
+	),
+	'resize'        => false,
+	'editor_height' => 150
+) );
 
 get_template_part( '/inc/builder/core/templates/overlay', 'footer' );
