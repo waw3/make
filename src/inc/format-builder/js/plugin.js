3fa3f01ebfc2a36463cb9de5c87eb74a7acfc1f3
Corey McKrill <cmckrill@jestro.com>
2014-09-16 14:34:42 -0800

    Rearrange option definitions

--- src/inc/format-builder/js/plugin.js
+++ src/inc/format-builder/js/plugin.js
@@ -1,24 +1,15 @@
 (function(tinymce) {
 	tinymce.PluginManager.add('ttfmake_format_builder', function( editor, url ) {
-		var formatFunctions = {
-				// @link http://stackoverflow.com/a/9756789/719811
-				escAttr: function(s, preserveCR) {
-					preserveCR = preserveCR ? '&#13;' : '\n';
-					return ('' + s) /* Forces the conversion to string. */
-						.replace(/&/g, '&amp;') /* This MUST be the 1st replacement. */
-						.replace(/'/g, '&apos;') /* The 4 other predefined entities, required. */
-						.replace(/"/g, '&quot;')
-						.replace(/</g, '&lt;')
-						.replace(/>/g, '&gt;')
-						.replace(/\r\n/g, preserveCR) /* Must be before the next replacement. */
-						.replace(/[\r\n]/g, preserveCR);
-				}
-			},
-			formatOptions = {
-				button: {
-					type: 'form',
-					name: 'optionsForm',
-					items: [
+		var formatWindow;
+
+		function getOptionFields( format, values ) {
+			var fields = {
+				type: 'form',
+				name: 'optionsForm'
+			};
+			switch(format) {
+				case 'button' :
+					fields.items = [
 						{
 							type: 'textbox',
 							name: 'buttonLabel',
@@ -50,45 +41,11 @@
 							type: 'textbox',
 							name: 'buttonPaddingY',
 							label: 'Padding (vertical)'
-						},
-						{
-							type: 'textbox',
-							name: 'buttonLabel1',
-							label: 'Label',
-							value: ''
-						},
-						{
-							type: 'textbox',
-							name: 'buttonURL1',
-							label: 'URL',
-							value: ''
-						},
-						{
-							type: 'textbox',
-							name: 'buttonColorBG1',
-							label: 'Background Color'
-						},
-						{
-							type: 'textbox',
-							name: 'buttonColorText1',
-							label: 'Text Color'
-						},
-						{
-							type: 'textbox',
-							name: 'buttonPaddingX1',
-							label: 'Padding (horizontal)'
-						},
-						{
-							type: 'textbox',
-							name: 'buttonPaddingY1',
-							label: 'Padding (vertical)'
 						}
-					]
-				},
-				alert: {
-					type: 'form',
-					name: 'optionsForm',
-					items: [
+					];
+					break;
+				case 'alert' :
+					fields.items = [
 						{
 							type: 'textbox',
 							multiline: true,
@@ -116,10 +73,12 @@
 							name: 'alertPaddingY',
 							label: 'Padding (vertical)'
 						}
-					]
-				}
-			},
-			formatWindow;
+					];
+					break;
+			}
+
+			return fields;
+		}
 
 		function openWindow() {
 			formatWindow = editor.windowManager.open( {
@@ -150,15 +109,15 @@
 										{ value: 'alert', text: 'Alert Box' }
 									],
 									onselect: function() {
-										var choice = this.value();
+										var choice = this.value(),
+											options = getOptionFields(choice);
 										formatWindow.find('#optionsForm').remove();
-										formatWindow.find('#formatContainer')[0].append(formatOptions[choice]).reflow();
+										formatWindow.find('#formatContainer')[0].append(options).reflow();
 										formatWindow.repaint();
 									}
 								}
 							]
-						},
-						formatOptions['button']
+						}
 					]
 				},
 				buttons: {
@@ -170,10 +129,26 @@
 			} );
 		}
 
+		function getState() {
+
+		}
+
 		editor.addButton('ttfmake_format_builder', {
 			icon: 'ttfmake-format-builder',
 			tooltip: 'Format Builder',
 			onclick: openWindow
 		});
+
+		function escAttr(s, preserveCR) {
+			preserveCR = preserveCR ? '&#13;' : '\n';
+			return ('' + s) /* Forces the conversion to string. */
+				.replace(/&/g, '&amp;') /* This MUST be the 1st replacement. */
+				.replace(/'/g, '&apos;') /* The 4 other predefined entities, required. */
+				.replace(/"/g, '&quot;')
+				.replace(/</g, '&lt;')
+				.replace(/>/g, '&gt;')
+				.replace(/\r\n/g, preserveCR) /* Must be before the next replacement. */
+				.replace(/[\r\n]/g, preserveCR);
+		}
 	});
 })(tinymce);
