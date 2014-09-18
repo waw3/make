38bae534a8802a0dc9463b05b7fab6fcdabfd731
Zack Tollman <tollmanz@gmail.com>
2014-09-17 21:08:14 -0700

    Use full height for editor.

--- src/inc/builder/core/templates/overlay-tinymce.php
+++ src/inc/builder/core/templates/overlay-tinymce.php
@@ -10,7 +10,7 @@ wp_editor( '', 'make', array(
 		'wp_autoresize_on' => false,
 	),
 	'resize'        => false,
-	'editor_height' => 220
+	'editor_height' => 320
 ) );
 
 get_template_part( '/inc/builder/core/templates/overlay', 'footer' );
