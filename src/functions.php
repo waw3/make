3f8d9409ac67c5b2a25cd59a1ded405891f4f0dc
Corey McKrill <cmckrill@jestro.com>
2014-11-11 11:17:20 -0900

    Update the Make Plus link

--- src/functions.php
+++ src/functions.php
@@ -566,6 +566,6 @@ add_action( 'customize_controls_print_styles', 'ttfmake_plus_styles', 20 );
  * @return string                   The link.
  */
 function ttfmake_get_plus_link( $deprecated ) {
-	$url = 'https://thethemefoundry.com/wordpress-themes/make/#make-table';
+	$url = 'https://thethemefoundry.com/make-buy/';
 	return esc_url( $url );
 }
