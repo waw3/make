184c4dacefd27c1a87c4e3507bb57d83fb086873
Corey McKrill <cmckrill@jestro.com>
2015-01-27 10:46:35 -0900

    Update FontAwesome to 4.3.0

--- Gruntfile.js
+++ Gruntfile.js
@@ -245,6 +245,18 @@ module.exports = function( grunt ) {
 						} );
 					} );
 
+					_.forEach( newObj, function( category ) {
+						category.sort( function( a, b ) {
+							if (a.name.toLowerCase() > b.name.toLowerCase()) {
+								return 1;
+							}
+							if (a.name.toLowerCase() < b.name.toLowerCase()) {
+								return -1;
+							}
+							return 0;
+						} );
+					} );
+
 					return newObj;
 				},
 				files: {
