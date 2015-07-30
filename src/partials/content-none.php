0e32eb3ec84bff1896ec68cbfea13524c32c015e
Corey McKrill <cmckrill@jestro.com>
2015-07-30 12:59:27 -0800

    Fix escaping on translation string
    Refs https://github.com/thethemefoundry/Make-Feedback/issues/21

--- src/partials/content-none.php
+++ src/partials/content-none.php
@@ -13,14 +13,7 @@
 
 	<div class="entry-content">
 		<?php if ( is_home() && current_user_can( 'publish_posts' ) ) : ?>
-		<p>
-			<?php
-			printf(
-				esc_html__( 'Ready to publish your first post? <a href="%1$s">Get started here</a>.', 'make' ),
-				esc_url( admin_url( 'post-new.php' ) )
-			);
-			?>
-		</p>
+		<p><?php printf( wp_kses( __( 'Ready to publish your first post? <a href="%1$s">Get started here</a>.', 'make' ), array( 'a' => array( 'href' => array() ) ) ), esc_url( admin_url( 'post-new.php' ) ) ); ?></p>
 		<?php elseif ( is_search() ) : ?>
 		<p><?php esc_html_e( 'Sorry, but nothing matched your search terms. Please try again with some different keywords.', 'make' ); ?></p>
 		<?php else : ?>
