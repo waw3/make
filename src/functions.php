d749174c12cfdc09b2a2bb7bb960a878d1eb745b
Corey McKrill <cmckrill@jestro.com>
2015-07-31 10:23:21 -0800

    Bump version to 1.6.2

--- src/functions.php
+++ src/functions.php
@@ -6,7 +6,7 @@
 /**
  * The current version of the theme.
  */
-define( 'TTFMAKE_VERSION', '1.6.1' );
+define( 'TTFMAKE_VERSION', '1.6.2' );
 
 /**
  * The minimum version of WordPress required for Finder.
