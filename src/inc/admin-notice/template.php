56a1d58d2754232648e57fddb6d211ca5044dd04
Corey McKrill <cmckrill@jestro.com>
2015-09-18 17:25:54 -0800

    Admin Notice: Use built-in dismissible class, many code improvements

--- src/inc/admin-notice/template.php
+++ src/inc/admin-notice/template.php
@@ -3,9 +3,9 @@
  * @package Make
  */
 ?>
-<div id="ttfmake-notice-<?php echo esc_attr( $id ); ?>" class="notice notice-<?php echo esc_attr( $type ); ?> <?php echo esc_attr( $legacy_class ); ?>">
-	<?php if ( true === $dismiss ) : ?>
-		<a class="ttfmake-dismiss" href="#" data-nonce="<?php echo esc_attr( $nonce ); ?>"><?php esc_html_e( 'Hide', 'make' ); ?></a>
+<div id="ttfmake-notice-<?php echo esc_attr( $id ); ?>" class="<?php echo esc_attr( $classes ); ?>" data-nonce="<?php echo esc_attr( $nonce ); ?>">
+	<?php if ( true === $dismiss && false === $this->support['dismissible'] ) : ?>
+		<a class="ttfmake-dismiss" href="#"><?php esc_html_e( 'Hide', 'make' ); ?></a>
 	<?php endif; ?>
 	<?php echo wpautop( $message ); ?>
 </div>
