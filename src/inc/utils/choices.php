b9087703bfc5e7bea0011051f2027e692a78f871
Corey McKrill <cmckrill@jestro.com>
2015-10-01 17:02:34 -0800

    Improvements to choices class

--- src/inc/utils/choices.php
+++ src/inc/utils/choices.php
@@ -8,47 +8,145 @@
  *
  * An object for defining and managing choice sets.
  *
- * @since 1.x.x.
+ * @since x.x.x.
  */
 class TTFMAKE_Utils_Choices {
 	/**
 	 * The collection of choice sets.
 	 *
-	 * @since 1.x.x.
+	 * @since x.x.x.
 	 *
 	 * @var array
 	 */
 	protected $choice_sets = array();
 
 	/**
-	 * Initialize the object.
-	 *
-	 * @since 1.x.x.
-	 */
-	final function __construct() {
-		$this->load();
-
-		/**
-		 * Action fires after the choices object's load method has been called.
-		 *
-		 * This action gives a developer the opportunity add or modify choice sets
-		 * and run additional load routines.
-		 *
-		 * @since 1.x.x.
-		 *
-		 * @param object    $choices    The choices object that has just finished loading.
-		 */
-		do_action( 'make_choices_loaded', $this );
-	}
-
-	/**
 	 * Set up the object.
 	 *
-	 * @since 1.x.x.
+	 * @since x.x.x.
 	 *
 	 * @return void
 	 */
-	protected function load() {}
+	protected function load() {
+		// Add the default choice sets.
+		$this->add_choice_sets( array(
+			'0-4' => array(
+				0 => __( '0', 'make' ),
+				1 => __( '1', 'make' ),
+				2 => __( '2', 'make' ),
+				3 => __( '3', 'make' ),
+				4 => __( '4', 'make' )
+			),
+			'alignment-horizontal-2' => array(
+				'left'  => __( 'Left', 'make' ),
+				'right' => __( 'Right', 'make' )
+			),
+			'alignment-horizontal-3' => array(
+				'left'   => __( 'Left', 'make' ),
+				'center' => __( 'Center', 'make' ),
+				'right'  => __( 'Right', 'make' )
+			),
+			'alignment-full-9' => array(
+				'top-left'      => __( 'Top Left', 'make' ),
+				'top'           => __( 'Top', 'make' ),
+				'top-right'     => __( 'Top Right', 'make' ),
+				'left'          => __( 'Left', 'make' ),
+				'center'        => __( 'Center', 'make' ),
+				'right'         => __( 'Right', 'make' ),
+				'bottom-left'   => __( 'Bottom Left', 'make' ),
+				'bottom'        => __( 'Bottom', 'make' ),
+				'bottom-right'  => __( 'Bottom Right', 'make' ),
+			),
+			'background-attachment' => array(
+				'scroll' => __( 'Scroll', 'make' ),
+				'fixed'  => __( 'Fixed', 'make' ),
+			),
+			'background-repeat' => array(
+				'no-repeat' => __( 'No Repeat', 'make' ),
+				'repeat'    => __( 'Tile', 'make' ),
+				'repeat-x'  => __( 'Tile Horizontally', 'make' ),
+				'repeat-y'  => __( 'Tile Vertically', 'make' )
+			),
+			'background-size' => array(
+				'auto'    => __( 'Auto', 'make' ),
+				'cover'   => __( 'Cover', 'make' ),
+				'contain' => __( 'Contain', 'make' )
+			),
+			'comment-count' => array(
+				'icon' => __( 'With icon', 'make' ),
+				'text' => __( 'With text', 'make' ),
+				'none' => __( 'None', 'make' ),
+			),
+			'featured-images' => array(
+				'post-header' => __( 'Post header', 'make' ),
+				'thumbnail'   => __( 'Thumbnail', 'make' ),
+				'none'        => __( 'None', 'make' ),
+			),
+			'font-style' => array(
+				'normal' => __( 'Normal', 'make' ),
+				'italic' => __( 'Italic', 'make' ),
+			),
+			'font-weight' => array(
+				'normal' => __( 'Normal', 'make' ),
+				'bold'   => __( 'Bold', 'make' ),
+			),
+			'footer-layout' => array(
+				1  => __( 'Traditional', 'make' ),
+				2  => __( 'Centered', 'make' ),
+			),
+			'general-layout' => array(
+				'full-width' => __( 'Full-width', 'make' ),
+				'boxed'      => __( 'Boxed', 'make' )
+			),
+			'header-bar-content-layout' => array(
+				'default' => __( 'Default', 'make' ),
+				'flipped' => __( 'Flipped', 'make' )
+			),
+			'header-layout' => array(
+				1  => __( 'Traditional', 'make' ),
+				2  => __( 'Centered', 'make' ),
+				3  => __( 'Navigation Below', 'make' ),
+			),
+			'link-underline' => array(
+				'always' => __( 'Always', 'make' ),
+				'hover'  => __( 'On hover/focus', 'make' ),
+				'never'  => __( 'Never', 'make' ),
+			),
+			'post-author' => array(
+				'avatar' => __( 'With avatar', 'make' ),
+				'name'   => __( 'Without avatar', 'make' ),
+				'none'   => __( 'None', 'make' ),
+			),
+			'post-date' => array(
+				// Translators: %s is a placeholder for a date.
+				'absolute' => sprintf( __( 'Absolute (%s)', 'make' ), date( get_option( 'date_format' ), time() - WEEK_IN_SECONDS ) ),
+				'relative' => __( 'Relative (1 week ago)', 'make' ),
+				'none'     => __( 'None', 'make' ),
+			),
+			'post-item-location' => array(
+				'top'            => __( 'Top', 'make' ),
+				'before-content' => __( 'Before content', 'make' ),
+				'post-footer'    => __( 'Post footer', 'make' ),
+			),
+			'text-transform' => array(
+				'none'      => __( 'None', 'make' ),
+				'uppercase' => __( 'Uppercase', 'make' ),
+				'lowercase' => __( 'Lowercase', 'make' ),
+			),
+		) );
+
+		/**
+		 * Action: Fires at the end of the choices object's load method.
+		 *
+		 * This action gives a developer the opportunity to add or modify choice sets
+		 * and run additional load routines.
+		 *
+		 * @since x.x.x.
+		 *
+		 * @param TTFMAKE_Utils_Choices    $choices    The choices object that has just finished loading.
+		 */
+		do_action( 'make_choices_loaded', $this );
+	}
 
 	/**
 	 * Add choice sets to the collection.
@@ -67,12 +165,12 @@ class TTFMAKE_Utils_Choices {
 	 *     ),
 	 * )
 	 *
-	 * @since 1.x.x.
+	 * @since x.x.x.
 	 *
 	 * @param          $sets         Array of choice sets to add.
 	 * @param  bool    $overwrite    True overwrites an existing choice set with the same ID.
 	 *
-	 * @return array|WP_Error        The modified array of choices if successful, otherwise an error object.
+	 * @return bool|WP_Error        True if addition was successful, otherwise an error object.
 	 */
 	public function add_choice_sets( $sets, $overwrite = false ) {
 		$sets = (array) $sets;
@@ -100,17 +198,17 @@ class TTFMAKE_Utils_Choices {
 		// Add the valid new choices sets to the existing choices array.
 		$this->choice_sets = array_merge( $existing_sets, $new_sets );
 
-		return $this->choice_sets;
+		return true;
 	}
 
 	/**
 	 * Remove choice sets from the collection.
 	 *
-	 * @since 1.x.x.
+	 * @since x.x.x.
 	 *
 	 * @param  array|string    $set_ids    The array of choice sets to remove, or 'all'.
 	 *
-	 * @return array|WP_Error              The modified array of choice sets if successful, otherwise an error object.
+	 * @return bool|WP_Error               True if removal was successful, otherwise an error object.
 	 */
 	public function remove_choice_sets( $set_ids ) {
 		if ( 'all' === $set_ids ) {
@@ -134,14 +232,14 @@ class TTFMAKE_Utils_Choices {
 			// No choice sets were removed.
 			return new WP_Error( 'make_choices_remove_choice_sets_none_removed', __( 'None of the specified choice sets were found in the collection, so none were removed.', 'make' ), $set_ids );
 		} else {
-			return $this->choice_sets;
+			return true;
 		}
 	}
 
 	/**
 	 * Get a particular choice set, using the set ID.
 	 *
-	 * @since 1.x.x.
+	 * @since x.x.x.
 	 *
 	 * @param  string    $set_id    The ID of the choice set to retrieve.
 	 *
@@ -156,9 +254,9 @@ class TTFMAKE_Utils_Choices {
 		}
 
 		/**
-		 * Filter the choices in a particular choice set.
+		 * Filter: Modify the choices in a particular choice set.
 		 *
-		 * @since 1.x.x.
+		 * @since x.x.x.
 		 *
 		 * @param array     $choice_set    The array of choices in the choice set.
 		 * @param string    $set_id        The ID of the choice set.
@@ -169,7 +267,7 @@ class TTFMAKE_Utils_Choices {
 	/**
 	 * Get the label of an individual choice in a choice set.
 	 *
-	 * @since 1.x.x.
+	 * @since x.x.x.
 	 *
 	 * @param  string    $set_id    The ID of the choice set.
 	 * @param  string    $choice    The array key representing the value of the choice.
@@ -182,13 +280,23 @@ class TTFMAKE_Utils_Choices {
 		}
 
 		$choices = $this->get_choice_set( $set_id );
-		return $choices[ $choice ];
+
+		/**
+		 * Filter: Modify the label for a particular choice value.
+		 *
+		 * @since x.x.x.
+		 *
+		 * @param string    $label     The label for the choice.
+		 * @param mixed     $choice    The value for the choice.
+		 * @param string    $set_id    The ID of the set that the choice belongs to.
+		 */
+		return apply_filters( 'make_choice_get_choice_label', $choices[ $choice ], $choice, $set_id );
 	}
 
 	/**
 	 * Determine if a value is a valid choice.
 	 *
-	 * @since 1.x.x.
+	 * @since x.x.x.
 	 *
 	 * @param  string    $set_id    The ID of the choice set.
 	 * @param  string    $choice    The array key representing the value of the choice.
@@ -203,7 +311,7 @@ class TTFMAKE_Utils_Choices {
 	/**
 	 * Sanitize a value from a list of allowed values in a choice set.
 	 *
-	 * @since 1.x.x.
+	 * @since x.x.x.
 	 *
 	 * @param  mixed     $value      The value given to sanitize.
 	 * @param  string    $set_id     The ID of the choice set to search for the given value.
