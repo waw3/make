2a5f66c3c326929b35ae8e126e73ac5544261f38
Corey McKrill <cmckrill@jestro.com>
2015-10-02 23:55:06 -0800

    Scaffold fonts class and interface

--- src/inc/util/fonts/fontsinterface.php
+++ src/inc/util/fonts/fontsinterface.php
@@ -2,3 +2,10 @@
 /**
  * @package Make
  */
+
+
+interface TTFMAKE_Util_Fonts_FontsInterface extends TTFMAKE_Util_LoadInterface {
+	public function get_font_data();
+
+	public function get_font_choices();
+}
