81c126808f0cfe106abb1c7502a2c9e72f17adf7
Corey McKrill <cmckrill@jestro.com>
2016-01-05 20:35:28 -0900

    Split environment assertions into separate tests

--- tests/tests_test.php
+++ tests/tests_test.php
@@ -7,10 +7,15 @@ class TEST_Make_Tests extends WP_UnitTestCase {
 	/**
 	 * @since x.x.x.
 	 */
-	function test_loaded_environment() {
+	function test_active_theme() {
 		// Make is the active theme
 		$this->assertTrue( 'make' === get_template() );
+	}
 
+	/**
+	 * @since x.x.x.
+	 */
+	function test_active_plugins() {
 		// Make Plus is enabled
 		$this->assertTrue( is_plugin_active( 'make-plus/make-plus.php' ) );
 	}
