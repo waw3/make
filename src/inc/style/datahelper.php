2c5abe43565fc9a21f0baa0b3a87e025a2f0fe76
Corey McKrill <cmckrill@jestro.com>
2016-03-23 15:59:59 -0800

    Style: restore deprecated relative font sizes that were removed

--- src/inc/style/datahelper.php
+++ src/inc/style/datahelper.php
@@ -176,10 +176,20 @@ class MAKE_Style_DataHelper extends MAKE_Util_Modules implements MAKE_Style_Data
 		 * @param array    $sizes    The array of relative sizes.
 		 */
 		$sizes = apply_filters( 'make_font_relative_size', array(
+			// Relative to navigation font size
+			'sub-menu'        => 93,  // Deprecated in 1.3.0.
 			// Relative to Header Bar icon size
 			'header-bar-icon' => 85,
 			'footer-icon'     => 85,
+			// Relative to widget font size
+			'widget-title'    => 100, // Deprecated in 1.5.0.
 			// Relative to header font size
+			'h1'              => 100, // Deprecated in 1.3.0.
+			'h2'              => 74,  // Deprecated in 1.3.0.
+			'h3'              => 52,  // Deprecated in 1.3.0.
+			'h4'              => 52,  // Deprecated in 1.3.0.
+			'h5'              => 35,  // Deprecated in 1.3.0.
+			'h6'              => 30,  // Deprecated in 1.3.0.
 			'post-title'      => 74,
 			// Relative to body font size
 			'comments'        => 88,
