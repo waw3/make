4abe4ece71a2a9352f1c5c75b8c3761467564df4
Corey McKrill <cmckrill@jestro.com>
2016-03-24 11:36:43 -0800

    Update scripts interface

--- src/inc/setup/scriptsinterface.php
+++ src/inc/setup/scriptsinterface.php
@@ -15,13 +15,13 @@ interface MAKE_Setup_ScriptsInterface extends MAKE_Util_ModulesInterface {
 
 	public function get_located_file_url( $file_names );
 
-	public function is_registered( $style_id, $type );
+	public function is_registered( $dependency_id, $type );
 
-	public function add_dependency( $style_id, $dependency_id, $type );
+	public function add_dependency( $recipient_id, $dependency_id, $type );
 
 	public function remove_dependency( $recipient_id, $dependency_id, $type );
 
 	public function update_version( $recipient_id, $version, $type );
 
-	public function get_url( $dep_id, $type );
+	public function get_url( $dependency_id, $type );
 }
