10891ef4e638ecddaec54f8c18450a95deb829ff
Corey McKrill <cmckrill@jestro.com>
2016-04-20 12:35:19 -0800

    WooCommerce: add backcompat measure for pluggable content wrapper actions

--- src/inc/integration/woocommerce.php
+++ src/inc/integration/woocommerce.php
@@ -52,6 +52,9 @@ final class MAKE_Integration_WooCommerce extends MAKE_Util_Modules implements MA
 		// After main content
 		add_action( 'woocommerce_after_main_content', array( $this, 'after_main_content' ) );
 
+		// Backcompat with old head actions
+		add_action( 'make_deprecated_function_run', array( $this, 'backcompat_main_content_actions' ) );
+
 		// Hooking has occurred.
 		self::$hooked = true;
 	}
@@ -132,5 +135,40 @@ final class MAKE_Integration_WooCommerce extends MAKE_Util_Modules implements MA
 		<?php
 		// Right sidebar
 		ttfmake_maybe_show_sidebar( 'right' );
+	}
+
+	/**
+	 * Backcompat for deprecated pluggable functions hooked to woocommerce_{}_main_content actions.
+	 *
+	 * This will fire if the Compatibility module's deprecated_function method is run, which will happen
+	 * if either of the deprecated functions have been plugged.
+	 *
+	 * @since 1.7.0.
+	 *
+	 * @param string $function
+	 *
+	 * @return void
+	 */
+	public function backcompat_main_content_actions( $function ) {
+		// Only run this in the proper hook context.
+		if ( 'make_deprecated_function_run' !== current_action() ) {
+			return;
+		}
+
+		// Don't bother if this is happening during woocommerce_before_main_content already.
+		if ( doing_action( 'woocommerce_before_main_content' ) || did_action( 'woocommerce_before_main_content' ) ) {
+			return;
+		}
+
+		// Before
+		if ( 'ttfmake_woocommerce_before_main_content' === $function && false === has_action( 'woocommerce_before_main_content', 'ttfmake_woocommerce_before_main_content' ) ) {
+			remove_action( 'woocommerce_before_main_content', array( $this, 'before_main_content' ) );
+			add_action( 'woocommerce_before_main_content', 'ttfmake_woocommerce_before_main_content' );
+		}
+		// After
+		else if ( 'ttfmake_woocommerce_after_main_content' === $function && false === has_action( 'woocommerce_after_main_content', 'ttfmake_woocommerce_after_main_content' ) ) {
+			remove_action( 'woocommerce_after_main_content', array( $this, 'after_main_content' ) );
+			add_action( 'woocommerce_after_main_content', 'ttfmake_woocommerce_after_main_content' );
+		}
 	}
 }
