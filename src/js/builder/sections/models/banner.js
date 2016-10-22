196ffd502063d3d7bd48b23f7797e9bdcb7e16e0
sixFingers <ignazio.setti@gmail.com>
2016-10-22 12:32:53 +0200

    Fix typos

--- src/js/builder/sections/models/banner.js
+++ src/js/builder/sections/models/banner.js
@@ -4,14 +4,14 @@ var oneApp = oneApp || {};
 (function (window, Backbone, $, _, oneApp) {
 	'use strict';
 
-	oneApp.BunnerModel = Backbone.Model.extend({
+	oneApp.BannerModel = Backbone.Model.extend({
 		defaults: {
 			'section-type': 'banner'
 		}
 	});
 
 	// Set up this model as a "no URL model" where data is not synced with the server
-	oneApp.BunnerModel.prototype.sync = function () { return null; };
-	oneApp.BunnerModel.prototype.fetch = function () { return null; };
-	oneApp.BunnerModel.prototype.save = function () { return null; };
+	oneApp.BannerModel.prototype.sync = function () { return null; };
+	oneApp.BannerModel.prototype.fetch = function () { return null; };
+	oneApp.BannerModel.prototype.save = function () { return null; };
 })(window, Backbone, jQuery, _, oneApp);
