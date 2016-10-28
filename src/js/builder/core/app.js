3a201bffe7b16fe8475bb81d7635ec18d324318d
sixFingers <ignazio.setti@gmail.com>
2016-10-28 12:50:48 +0200

    Wait for page load before initializing builder

--- src/js/builder/core/app.js
+++ src/js/builder/core/app.js
@@ -425,7 +425,9 @@ var oneApp = oneApp || {}, ttfMakeFrames = ttfMakeFrames || [];
 	// Leaving function to avoid errors if 3rd party code uses it. Deprecated in 1.4.0.
 	oneApp.initAllEditors = function(id, model) {};
 
-	oneApp.initSortables();
-	oneApp.initViews();
-	oneApp.triggerInitFrames();
+	$(document).ready(function() {
+		oneApp.initSortables();
+		oneApp.initViews();
+		oneApp.triggerInitFrames();
+	})
 })(jQuery, oneApp, ttfMakeFrames);
