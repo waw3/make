ed3e8c97446876ffb0f4277b9b55d5fb268150bc
sixFingers <ignazio.setti@gmail.com>
2016-11-09 10:47:28 +0100

    Cleanup

--- src/inc/builder/core/templates/section-footer.php
+++ src/inc/builder/core/templates/section-footer.php
@@ -3,12 +3,13 @@
  * @package Make
  */
 
-global $ttfmake_section_data;
+global $ttfmake_section_data, $ttfmake_overlay_id;
+
+$ttfmake_overlay_id = 'ttfmake-overlay-{{ id }}';
 ?>
-<?php if ( ! empty( $ttfmake_section_data['section']['config'] ) ) : ?>
-	<?php global $ttfmake_overlay_id; $id = '{{ id }}'; $ttfmake_overlay_id = 'ttfmake-overlay-' . $id; ?>
+
 	<?php get_template_part( '/inc/builder/core/templates/overlay', 'configuration' ); ?>
 
     <textarea name="ttfmake-section-json[{{ id }}]" style="display: none;">{{ JSON.stringify(toJSON()) }}</textarea>
-<?php endif; ?>
+
 </div>
