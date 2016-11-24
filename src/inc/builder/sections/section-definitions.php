8888bbfe9f02a816f29256feced1380b5448db82
sixFingers <ignazio.setti@gmail.com>
2016-11-24 14:59:43 +0100

    Move Columns section to a class of its own

--- src/inc/builder/sections/section-definitions.php
+++ src/inc/builder/sections/section-definitions.php
@@ -45,7 +45,8 @@ class TTFMAKE_Section_Definitions {
 	 */
 	public function __construct() {
 		// Register all of the sections via the section API
-		$this->register_text_section();
+		$text_section = MAKE_Builder_Sections_Columns_Definition::register();
+
 		$this->register_banner_section();
 		$this->register_gallery_section();
 
@@ -57,166 +58,6 @@ class TTFMAKE_Section_Definitions {
 
 		// Add additional templating
 		add_action( 'admin_footer', array( $this, 'print_templates' ) );
-	}
-
-	/**
-	 * Register the text section.
-	 *
-	 * Note that in 1.4.0, the "text" section was renamed to "columns". In order to provide good back compatibility,
-	 * only the section label is changed to "Columns". All other internal references for this section will remain as
-	 * "text".
-	 *
-	 * @since  1.0.0.
-	 *
-	 * @return void
-	 */
-	public function register_text_section() {
-		ttfmake_add_section(
-			'text',
-			__( 'Columns', 'make' ),
-			Make()->scripts()->get_css_directory_uri() . '/builder/sections/images/text.png',
-			__( 'Create rearrangeable columns of content and images.', 'make' ),
-			array( $this, 'save_text' ),
-			'sections/builder-templates/text',
-			'sections/front-end-templates/text',
-			100,
-			get_template_directory() . '/inc/builder/',
-			array(
-				100 => array(
-					'type'  => 'section_title',
-					'name'  => 'title',
-					'label' => __( 'Enter section title', 'make' ),
-					'class' => 'ttfmake-configuration-title ttfmake-section-header-title-input',
-				),
-				200 => array(
-					'type'    => 'select',
-					'name'    => 'columns-number',
-					'class'   => 'ttfmake-text-columns',
-					'label'   => __( 'Columns', 'make' ),
-					'default' => 3,
-					'options' => array(
-						1 => 1,
-						2 => 2,
-						3 => 3,
-						4 => 4,
-					),
-				),
-				300 => array(
-					'type'  => 'image',
-					'name'  => 'background-image',
-					'label' => __( 'Background image', 'make' ),
-					'class' => 'ttfmake-configuration-media'
-				),
-				400 => array(
-					'type'    => 'checkbox',
-					'label'   => __( 'Darken background to improve readability', 'make' ),
-					'name'    => 'darken',
-					'default' => 0,
-				),
-				500 => array(
-					'type'    => 'select',
-					'name'    => 'background-style',
-					'label'   => __( 'Background style', 'make' ),
-					'default' => 'tile',
-					'options' => array(
-						'tile'  => __( 'Tile', 'make' ),
-						'cover' => __( 'Cover', 'make' ),
-					),
-				),
-				600 => array(
-					'type'    => 'color',
-					'label'   => __( 'Background color', 'make' ),
-					'name'    => 'background-color',
-					'class'   => 'ttfmake-text-background-color ttfmake-configuration-color-picker',
-					'default' => ''
-				),
-			)
-		);
-	}
-
-	/**
-	 * Save the data for the text section.
-	 *
-	 * @since  1.0.0.
-	 *
-	 * @param  array    $data    The data from the $_POST array for the section.
-	 * @return array             The cleaned data.
-	 */
-	public function save_text( $data ) {
-		$clean_data = array();
-
-		if ( isset( $data['columns-number'] ) ) {
-			if ( in_array( $data['columns-number'], range( 1, 4 ) ) ) {
-				$clean_data['columns-number'] = $data['columns-number'];
-			}
-		}
-
-		$clean_data['title'] = $clean_data['label'] = ( isset( $data['title'] ) ) ? apply_filters( 'title_save_pre', $data['title'] ) : '';
-
-		if ( isset( $data['columns-order'] ) ) {
-			$clean_data['columns-order'] = $data['columns-order'];
-		}
-
-		if ( isset( $data['background-image'] ) ) {
-			$clean_data['background-image'] = ttfmake_sanitize_image_id( $data['background-image'] );
-		}
-
-		if ( isset( $data['darken'] ) && $data['darken'] == 1 ) {
-			$clean_data['darken'] = 1;
-		} else {
-			$clean_data['darken'] = 0;
-		}
-
-		if ( isset( $data['background-color'] ) ) {
-			$clean_data['background-color'] = maybe_hash_hex_color( $data['background-color'] );
-		}
-
-		if ( isset( $data['background-style'] ) ) {
-			if ( in_array( $data['background-style'], array( 'tile', 'cover' ) ) ) {
-				$clean_data['background-style'] = $data['background-style'];
-			}
-		}
-
-		if ( isset( $data['columns'] ) && is_array( $data['columns'] ) ) {
-			foreach ( $data['columns'] as $id => $item ) {
-				if ( isset( $item['id'] ) ) {
-					$clean_data['columns'][ $id ]['id'] = $item['id'];
-				}
-
-				if ( isset( $item['parentID'] ) ) {
-					$clean_data['columns'][ $id ]['parentID'] = $item['parentID'];
-				}
-
-				if ( isset( $item['title'] ) ) {
-					$clean_data['columns'][ $id ]['title'] = apply_filters( 'title_save_pre', $item['title'] );
-				}
-
-				if ( isset( $item['image-link'] ) ) {
-					$clean_data['columns'][ $id ]['image-link'] = esc_url_raw( $item['image-link'] );
-				}
-
-				if ( isset( $item['image-id'] ) ) {
-					$clean_data['columns'][ $id ]['image-id'] = ttfmake_sanitize_image_id( $item['image-id'] );
-
-					$image = ttfmake_get_image_src( $item['image-id'], 'large' );
-					$clean_data['columns'][ $id ]['image-url'] = $image[0];
-				}
-
-				if ( isset( $item['content'] ) ) {
-					$clean_data['columns'][ $id ]['content'] = sanitize_post_field( 'post_content', $item['content'], ( get_post() ) ? get_the_ID() : 0, 'db' );
-				}
-
-				if ( isset( $item['size'] ) ) {
-					$clean_data['columns'][ $id ]['size'] = esc_attr( $item['size'] );
-				}
-
-				if ( isset( $item['sidebar-label'] ) ) {
-					$clean_data['columns'][ $id ]['sidebar-label'] = $item['sidebar-label'];
-				}
-			}
-		}
-
-		return $clean_data;
 	}
 
 	/**
@@ -820,38 +661,6 @@ class TTFMAKE_Section_Definitions {
 		);
 
 		wp_register_script(
-			'ttfmake-sections/js/models/text.js',
-			Make()->scripts()->get_js_directory_uri() . '/builder/sections/models/text.js',
-			array(),
-			TTFMAKE_VERSION,
-			true
-		);
-
-		wp_register_script(
-			'ttfmake-sections/js/views/text.js',
-			Make()->scripts()->get_js_directory_uri() . '/builder/sections/views/text.js',
-			array(),
-			TTFMAKE_VERSION,
-			true
-		);
-
-		wp_register_script(
-			'ttfmake-sections/js/models/text-item.js',
-			Make()->scripts()->get_js_directory_uri() . '/builder/sections/models/text-item.js',
-			array(),
-			TTFMAKE_VERSION,
-			true
-		);
-
-		wp_register_script(
-			'ttfmake-sections/js/views/text-item.js',
-			Make()->scripts()->get_js_directory_uri() . '/builder/sections/views/text-item.js',
-			array(),
-			TTFMAKE_VERSION,
-			true
-		);
-
-		wp_register_script(
 			'ttfmake-sections/js/models/banner.js',
 			Make()->scripts()->get_js_directory_uri() . '/builder/sections/models/banner.js',
 			array(),
@@ -925,10 +734,6 @@ class TTFMAKE_Section_Definitions {
 			'ttfmake-sections/js/models/banner-slide.js',
 			'ttfmake-sections/js/views/gallery-item.js',
 			'ttfmake-sections/js/views/gallery.js',
-			'ttfmake-sections/js/models/text.js',
-			'ttfmake-sections/js/models/text-item.js',
-			'ttfmake-sections/js/views/text-item.js',
-			'ttfmake-sections/js/views/text.js',
 			'ttfmake-sections/js/views/banner-slide.js',
 			'ttfmake-sections/js/views/banner.js',
 		) );
@@ -1029,12 +834,7 @@ class TTFMAKE_Section_Definitions {
  	 * @return array                      The array of choices for the section setting.
 	 */
 	public function get_choices( $key, $section_type ) {
-		$choices = array( 0 );
-
-		$choice_id = "$section_type-$key";
-
-		// Note that this function does not do anything yet. It is part of an API refresh that is happening over time.
-		switch ( $choice_id ) {}
+		$choices = array();
 
 		/**
 		 * Filter the section choices.
@@ -1062,8 +862,11 @@ if ( ! function_exists( 'ttfmake_get_section_default' ) ) :
  */
 function ttfmake_get_section_default( $key, $section_type ) {
 	$defaults = ttfmake_get_section_definitions()->get_section_defaults();
-	$id       = "$section_type-$key";
-	$value    = ( isset( $defaults[ $id ] ) ) ? $defaults[ $id ] : false;
+	$value = false;
+
+	if ( isset( $defaults[$section_type] ) && isset( $defaults[$section_type][$key] ) ) {
+		$value = $defaults[$section_type][$key];
+	}
 
 	/**
 	 * Filter the default section data that is received.
