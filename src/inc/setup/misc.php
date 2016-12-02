73f329641d243026eb517aa33e54655effbb3c4e
Tomas Stark <hi@tomasstark.rocks>
2016-12-02 09:31:36 +0100

    Remove `customize-selective-refresh-widgets` to disable shortcuts altogether

--- src/inc/setup/misc.php
+++ src/inc/setup/misc.php
@@ -119,9 +119,6 @@ final class MAKE_Setup_Misc extends MAKE_Util_Modules implements MAKE_Setup_Misc
 			'flex-width'  => true,
 		) );
 
-		// Customizer: selective refresh for widgets
-		add_theme_support( 'customize-selective-refresh-widgets' );
-
 		// HTML5
 		add_theme_support( 'html5', array(
 			'comment-list',
