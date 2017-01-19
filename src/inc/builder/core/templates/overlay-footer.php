668b5f7408f03893c112661626ef08237694967d
sixFingers <ignazio.setti@gmail.com>
2017-01-19 10:56:52 +0100

    More standard-looking overlay

--- src/inc/builder/core/templates/overlay-footer.php
+++ src/inc/builder/core/templates/overlay-footer.php
@@ -1,3 +1,6 @@
 		</div>
+		<div class="ttfmake-overlay-footer">
+			<span class="ttfmake-overlay-close ttfmake-overlay-close-action button media-button button-primary button-large media-button-insert" aria-hidden="true" data-original-text="<?php esc_html_e( 'Update changes', 'make' ); ?>"><?php esc_html_e( 'Update changes', 'make' ); ?></span>
+		</div>
 	</div>
 </div>
