f33070e48b50b157c57c32db73ba40bac6f25f15
sixFingers <ignazio.setti@gmail.com>
2017-02-02 10:54:33 +0100

    Remove 'Cancel settings' button from overlay footer

--- src/inc/builder/core/templates/overlay-footer.php
+++ src/inc/builder/core/templates/overlay-footer.php
@@ -1,6 +1,5 @@
 			</div>
 			<div class="ttfmake-overlay-footer">
-				<span class="ttfmake-overlay-close-discard button button-secondary button-large" aria-hidden="true" data-original-text="<?php esc_html_e( 'Update changes', 'make' ); ?>"><?php esc_html_e( 'Discard changes', 'make' ); ?></span>
 				<span class="ttfmake-overlay-close-update button button-primary button-large" aria-hidden="true" data-original-text="<?php esc_html_e( 'Update changes', 'make' ); ?>"><?php esc_html_e( 'Update changes', 'make' ); ?></span>
 			</div>
 		</div>
